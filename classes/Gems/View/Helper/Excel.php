267f2acc2a6fb42e24932fec80f76616b9a9c135
Menno Dekker <mennodekker@users.sourceforge.net>
2011-09-26 10:07:56 +0000

    backported from trunk spss and excel export fixes

--- classes/Gems/View/Helper/Excel.php
+++ classes/Gems/View/Helper/Excel.php
@@ -3,7 +3,7 @@
 /**
  * Copyright (c) 2011, Erasmus MC
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *    * Redistributions of source code must retain the above copyright
@@ -14,7 +14,7 @@
  *    * Neither the name of Erasmus MC nor the
  *      names of its contributors may be used to endorse or promote products
  *      derived from this software without specific prior written permission.
- *      
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -29,7 +29,7 @@
 
 /**
  * Outputs an array of arrays (or a Zend_Db_Table_Rowset) as a table
- * 
+ *
  * The first 'record' is rendered bold, being the header for the table
  *
  * @category    Gems
@@ -49,25 +49,35 @@ class Gems_View_Helper_Excel extends Zend_View_Helper_Abstract
             Zend_Controller_Front::getInstance()->unregisterPlugin('ZFDebug_Controller_Plugin_Debug');
         } catch (Exception $e) {}
         $this->view->layout()->setLayout('excel');
+        if ($rowset instanceof Gems_FormattedData) {
+            $rowset->setFormatted(false);
+        }
         $rowcnt = 0;
         foreach ($rowset as $row) {
-            if ($row instanceof Zend_Db_Table_Row) $row = $row->toArray();
-            if (!is_array($row)) $row = (array) $row;
+            if ($row instanceof Zend_Db_Table_Row) {
+                $row = $row->toArray();
+            }
+            if (!is_array($row)) {
+                $row = (array) $row;
+            }
             if ($rowcnt == 0) {
                 //Only for the first row: output headers
                 $output = "<table>\r\n";
                 $output .= "\t<thead>\r\n";
                 $output .= "\t\t<tr>\r\n";
                 foreach ($row as $name => $value) {
-                    $output .= "\t\t\t<th>$value</th>\r\n"; 
+                    $output .= "\t\t\t<th>$value</th>\r\n";
                 }
                 $output .= "\t\t</tr>\r\n";
                 $output .= "\t</thead>\r\n";
                 $output .= "\t<tbody>\r\n";
-            } else {            
-                $output .= "\t\t<tr>\r\n";            
+                if ($rowset instanceof Gems_FormattedData) {
+                    $rowset->setFormatted(true);
+                }
+            } else {
+                $output .= "\t\t<tr>\r\n";
                 foreach ($row as $name => $value) {
-                    $output .= "\t\t\t<td>$value</td>\r\n"; 
+                    $output .= "\t\t\t<td>$value</td>\r\n";
                 }
                 $output .= "\t\t</tr>\r\n";
             }
@@ -76,7 +86,7 @@ class Gems_View_Helper_Excel extends Zend_View_Helper_Abstract
         if (isset($output)) {
             $output .= "\t</tbody>\r\n";
             $output .= "</table>\r\n";
-            return $output;            
+            return $output;
         } else {
             return null;
         }
