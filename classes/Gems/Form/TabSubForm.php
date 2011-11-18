234cf68effcaaf71c4e2b4c7e75dd73e709d4daf
Menno Dekker <mennodekker@users.sourceforge.net>
2011-11-18 13:24:26 +0000

    Fixed tabsubform to allow ->h4('title') calls for backward compatibility

--- classes/Gems/Form/TabSubForm.php
+++ classes/Gems/Form/TabSubForm.php
@@ -35,7 +35,39 @@
  * @package Gems
  * @subpackage Form
  */
-class Gems_Form_TabSubForm extends Gems_Form_TableForm {
+class Gems_Form_TabSubForm extends Gems_Form_TableForm
+{
+    /**
+     * For backward compatibility, allow MUtil_Html calls to set or append to the title
+     *
+     * @param type $method
+     * @param type $args
+     * @return Gems_Form_TabSubForm
+     */
+    public function __call($method, $args)
+    {
+        if ('render' == substr($method, 0, 6)) {
+            return parent::__call($method, $args);
+        }
+
+        $elem = MUtil_Html::createArray($method, $args);
+
+        $value = $this->getAttrib('title');
+
+        $value .= $elem->render($this->getView());
+
+        $this->setAttrib('title', $value);
+
+        //As the decorator might have been added already, update it also
+        $decorator = $this->getDecorator('TabPane');
+        $options   = $decorator->getOption('jQueryParams');
+
+        $options['title'] = strip_tags($value);
+
+        $decorator->setOption('jQueryParams', $options);
+
+        return $this;
+    }
     
     /**
      * Load default decorators
