94df58e6c2a2c281768eb185e7b663186c416ee4
Michiel Rook <michiel@touchdownconsulting.nl>
2011-11-19 18:12:07 +0000

    IP address column should be varchar

--- configs/db/tables/gems__token_attempts.10.sql
+++ configs/db/tables/gems__token_attempts.10.sql
@@ -2,7 +2,7 @@
 CREATE TABLE if not exists gems__token_attempts (
         gta_id_attempt bigint unsigned not null auto_increment,
         gta_id_token varchar(9) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' not null,
-        gta_ip_address int(11) unsigned not null,
+        gta_ip_address varchar(64) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' not null,
         gta_datetime timestamp not null default current_timestamp,
 
         PRIMARY KEY (gta_id_attempt)
