611b313f45548c239177ab29b44be4558b39f17c
Menno Dekker <mennodekker@users.sourceforge.net>
2011-12-15 11:34:17 +0000

    And the staffmodel ofcourse :)

---
+++ classes/Gems/Model/StaffModel.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Copyright (c) 2011, Erasmus MC
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the name of Erasmus MC nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *      
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The staff model
+ *
+ * @package    Gems
+ * @subpackage Model
+ * @copyright  Copyright (c) 2011 Erasmus MC
+ * @license    New BSD License
+ * @version    $Id: Sample.php 215 2011-07-12 08:52:54Z michiel $
+ */
+
+/**
+ * Contains the staffModel
+ *
+ * Handles saving of the password to the right userclass
+ *
+ * @package    Gems
+ * @subpackage Model
+ * @copyright  Copyright (c) 2011 Erasmus MC
+ * @license    New BSD License
+ * @since      Class available since version 1.5
+ */
+class Gems_Model_StaffModel extends Gems_Model_JoinModel implements MUtil_Registry_TargetInterface
+{
+    /**
+     * @var Gems_Loader
+     */
+    public $loader;
+
+    public function __construct()
+    {
+        parent::__construct('staff', 'gems__staff', 'gsf');
+    }
+
+    /**
+     * Allows the loader to set resources.
+     *
+     * @param string $name Name of resource to set
+     * @param mixed $resource The resource.
+     * @return boolean True if $resource was OK
+     */
+    public function answerRegistryRequest($name, $resource)
+    {
+        $this->$name = $resource;
+
+        return true;
+    }
+
+    /**
+     * Should be called after answering the request to allow the Target
+     * to check if all required registry values have been set correctly.
+     *
+     * @return boolean False if required are missing.
+     */
+    public function checkRegistryRequestsAnswers()
+    {
+        return true;
+    }
+
+    /**
+     * Filters the names that should not be requested.
+     *
+     * Can be overriden.
+     *
+     * @param string $name
+     * @return boolean
+     */
+    protected function filterRequestNames($name)
+    {
+        return '_' !== $name[0];
+    }
+
+    /**
+     * Allows the loader to know the resources to set.
+     *
+     * Returns those object variables defined by the subclass but not at the level of this definition.
+     *
+     * Can be overruled.
+     *
+     * @return array of string names
+     */
+    public function getRegistryRequests()
+    {
+        return array_filter(array_keys(get_object_vars($this)), array($this, 'filterRequestNames'));
+    }
+
+    /**
+     * Save a single model item.
+     *
+     * Makes sure the password is saved too using the userclass
+     *
+     * @param array $newValues The values to store for a single model item.
+     * @param array $filter If the filter contains old key values these are used
+     * to decide on update versus insert.
+     * @param array $saveTables Optional array containing the table names to save,
+     * otherwise the tables set to save at model level will be saved.
+     * @return array The values as they are after saving (they may change).
+     */
+    public function save(array $newValues, array $filter = null, array $saveTables = null)
+    {
+        //First perform a save
+        $savedValues = parent::save($newValues, $filter, $saveTables);
+
+        //Now check if we need to set the password
+        if(isset($newValues['fld_password']) && !empty($newValues['fld_password'])) {
+            if ($this->getChanged()<1) {
+                $this->setChanged(1);
+            }
+            //Now load the userclass and save the password
+            $user = $this->loader->getUserLoader()->getUserByStaffId($newValues['gsf_id_user']);
+            if ($user->canSetPassword()) {
+                $user->setPassword($newValues['fld_password']);
+            }
+        }
+
+        return $savedValues;
+    }
+}
