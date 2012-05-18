8b99e98f0eb06b4001811e7ca11142b98e29c698
Michiel Rook <michiel@touchdownconsulting.nl>
2012-05-18 14:38:23 +0000

    Move PDF version validation to MUtil_Validate_Pdf

--- classes/Gems/Default/SurveyMaintenanceAction.php
+++ classes/Gems/Default/SurveyMaintenanceAction.php
@@ -130,7 +130,8 @@ class Gems_Default_SurveyMaintenanceAction extends Gems_Controller_BrowseEditAct
                 'destination', $this->loader->getPdf()->getUploadDir('survey_pdfs'),
                 'extension', 'pdf',
                 'filename', $data['gsu_id_survey'],
-                'required', false);
+                'required', false)
+               ->addValidator(new MUtil_Validate_Pdf());
 
         $bridge->addExhibitor(  'track_count', 'label', $this->_('Usage'), 'value', $data['track_count']);
 
@@ -242,21 +243,12 @@ class Gems_Default_SurveyMaintenanceAction extends Gems_Controller_BrowseEditAct
         }
 
         // Set the value of the field in the database.
-        $data['gsu_survey_pdf'] = null;
         $new_name = $data['gsu_id_survey'] . '.pdf';
-        $pdfSource = $form->new_pdf->getDestination() . DIRECTORY_SEPARATOR . $new_name;
         
-        if (file_exists($pdfSource)) {
-            $objFactory = Zend_Pdf_ElementFactory::createFactory(1);
-            $parser = new Zend_Pdf_Parser($pdfSource, $objFactory, true);
-            $version = $parser->getPDFVersion();
-            
-            if (version_compare($version, '1.4', '>')) {
-                $this->addMessage(sprintf($this->_('Unsupported PDF version %s - only versions 1.0 - 1.4 are supported.'), $version));
-                return false;
-            }
-            
+        if (file_exists($form->new_pdf->getDestination() . DIRECTORY_SEPARATOR . $new_name)) {
             $data['gsu_survey_pdf'] = $new_name;
+        } else {
+            $data['gsu_survey_pdf'] = null;
         }
         
         $data['gtr_track_class'] = 'SingleSurveyEngine';
