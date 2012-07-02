f9d55a8734a16b5e29a3632439a3359b8cf794fe
Matijs de Jong <mjong@magnafacta.nl>
2012-07-02 15:17:02 +0000

    h1 up to h6 are now only rendered when they contain content

---
+++ classes/MUtil/Html/HnElement.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Copyright (c) 2012, Erasmus MC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the name of Erasmus MC nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * @package    MUtil
+ * @subpackage Html
+ * @author     Matijs de Jong <mjong@magnafacta.nl>
+ * @copyright  Copyright (c) 2012 Erasmus MC
+ * @license    New BSD License
+ * @version    $id: HnElement.php 203 2012-01-01t 12:51:32Z matijs $
+ */
+
+/**
+ *
+ *
+ * @package    MUtil
+ * @subpackage Html
+ * @copyright  Copyright (c) 2012 Erasmus MC
+ * @license    New BSD License
+ * @since      Class available since version 1.5.5
+ */
+class MUtil_Html_HnElement extends MUtil_Html_HtmlElement
+{
+    /**
+     * Most elements must be rendered even when empty, others should - according to the
+     * xhtml specifications - only be rendered when the element contains some content.
+     *
+     * $renderWithoutContent controls this rendering. By default an element tag is output
+     * but when false the tag will only be present if there is some content in it.
+     *
+     * @var boolean The element is rendered even without content when true.
+     */
+    public $renderWithoutContent = false;
+
+    /**
+     * Static helper function for creation, used by @see MUtil_Html_Creator.
+     *
+     * @param mixed $arg_array Optional MUtil_Ra::args processed settings
+     * @return MUtil_Html_TrElement
+     */
+    public static function h1($arg_array = null)
+    {
+        $args = func_get_args();
+        return new self(__FUNCTION__, $args);
+    }
+
+    /**
+     * Static helper function for creation, used by @see MUtil_Html_Creator.
+     *
+     * @param mixed $arg_array Optional MUtil_Ra::args processed settings
+     * @return MUtil_Html_TrElement
+     */
+    public static function h2($arg_array = null)
+    {
+        $args = func_get_args();
+        return new self(__FUNCTION__, $args);
+    }
+
+    /**
+     * Static helper function for creation, used by @see MUtil_Html_Creator.
+     *
+     * @param mixed $arg_array Optional MUtil_Ra::args processed settings
+     * @return MUtil_Html_TrElement
+     */
+    public static function h3($arg_array = null)
+    {
+        $args = func_get_args();
+        return new self(__FUNCTION__, $args);
+    }
+
+    /**
+     * Static helper function for creation, used by @see MUtil_Html_Creator.
+     *
+     * @param mixed $arg_array Optional MUtil_Ra::args processed settings
+     * @return MUtil_Html_TrElement
+     */
+    public static function h4($arg_array = null)
+    {
+        $args = func_get_args();
+        return new self(__FUNCTION__, $args);
+    }
+
+    /**
+     * Static helper function for creation, used by @see MUtil_Html_Creator.
+     *
+     * @param mixed $arg_array Optional MUtil_Ra::args processed settings
+     * @return MUtil_Html_TrElement
+     */
+    public static function h5($arg_array = null)
+    {
+        $args = func_get_args();
+        return new self(__FUNCTION__, $args);
+    }
+
+    /**
+     * Static helper function for creation, used by @see MUtil_Html_Creator.
+     *
+     * @param mixed $arg_array Optional MUtil_Ra::args processed settings
+     * @return MUtil_Html_TrElement
+     */
+    public static function h6($arg_array = null)
+    {
+        $args = func_get_args();
+        return new self(__FUNCTION__, $args);
+    }
+}
--- classes/MUtil/Html/TrElement.php
+++ classes/MUtil/Html/TrElement.php
@@ -1,10 +1,9 @@
 <?php
 
-
 /**
  * Copyright (c) 2011, Erasmus MC
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *    * Redistributions of source code must retain the above copyright
@@ -15,7 +14,7 @@
  *    * Neither the name of Erasmus MC nor the
  *      names of its contributors may be used to endorse or promote products
  *      derived from this software without specific prior written permission.
- *      
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -26,22 +25,23 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    MUtil
+ * @subpackage Html
+ * @author     Matijs de Jong <mjong@magnafacta.nl>
+ * @copyright  Copyright (c) 2012 Erasmus MC
+ * @license    New BSD License
+ * @version    $id: HnElement.php 203 2012-01-01t 12:51:32Z matijs $
  */
 
 /**
+ *
  * 
- * @author Matijs de Jong
- * @since 1.0
- * @version 1.1
- * @package MUtil
+ * @package    MUtil
  * @subpackage Html
- */
-
-/**
- * 
- * @author Matijs de Jong
- * @package MUtil
- * @subpackage Html
+ * @copyright  Copyright (c) 2012 Erasmus MC
+ * @license    New BSD License
+ * @since      Class available since version 1.0
  */
 class MUtil_Html_TrElement extends MUtil_Html_HtmlElement implements MUtil_Html_ColumnInterface
 {
@@ -77,19 +77,19 @@ class MUtil_Html_TrElement extends MUtil_Html_HtmlElement implements MUtil_Html_
     protected $_appendString = "\n";
 
     /**
-     * When repeating content using $_repeater you may want to output the content only when it has 
+     * When repeating content using $_repeater you may want to output the content only when it has
      * changed.
-     * 
+     *
      * @see $_repeater
-     * 
+     *
      * @var boolean Do not output if the output is identical to the last time the element was rendered.
      */
     protected $_onlyWhenChanged = false;
 
-    
+
     /**
      * @see $_onlyWhenChanged
-     * 
+     *
      * @var string Cache for last output for comparison
      */
     protected $_onlyWhenChangedValueStore = null;
@@ -97,29 +97,29 @@ class MUtil_Html_TrElement extends MUtil_Html_HtmlElement implements MUtil_Html_
 
     /**
      * Returns the cell or a MUtil_MultiWrapper containing cells that occupy the column position, taking colspan and other functions into account.
-     * 
+     *
      * @param int $col The numeric column position, starting at 0;
      * @return MUtil_Html_HtmlElement Probably an element of this type, but can also be something else, posing as an element.
      */
     public function getColumn($col)
     {
         $results = $this->getColumnArray($col);
-        
+
         switch (count($results)) {
             case 0:
                 return null;
-                
+
             case 1:
                 return reset($results);
-                
+
             default:
                 return new MUtil_MultiWrapper($results);
         }
     }
-    
+
     /**
      * Returns the cells that occupies the column position, taking colspan and other functions into account, in an array.
-     * 
+     *
      * @param int $col The numeric column position, starting at 0;
      * @return array Of probably one MUtil_Html_HtmlElement
      */
@@ -127,10 +127,10 @@ class MUtil_Html_TrElement extends MUtil_Html_HtmlElement implements MUtil_Html_
     {
         return array($this->getColumn($col));
     }
-    
+
     /**
      * Return the number of columns, taking such niceties as colspan into account
-     * 
+     *
      * @return int
      */
     public function getColumnCount()
@@ -146,20 +146,20 @@ class MUtil_Html_TrElement extends MUtil_Html_HtmlElement implements MUtil_Html_
 
     /**
      * Returns the cell's column width. A utility function.
-     * 
+     *
      * @param mixed $cell MUtil_Html_ColumnInterface
      * @return int
      */
-    public static function getCellWidth($cell) 
+    public static function getCellWidth($cell)
     {
         if ($cell instanceof MUtil_Html_ColumnInterface) {
             return $cell->getColumnCount();
-        } 
-        
+        }
+
         if (isset($cell->colspan) && is_int($cell->colspan)) {
             return  intval($cell->colspan);
-        } 
-        
+        }
+
         // Assume it is a single column
         return 1;
     }
@@ -171,9 +171,9 @@ class MUtil_Html_TrElement extends MUtil_Html_HtmlElement implements MUtil_Html_
 
     /**
      * Function to allow overloading  of tag rendering only
-     * 
+     *
      * Renders the element tag with it's content into a html string
-     * 
+     *
      * The $view is used to correctly encode and escape the output
      *
      * @param Zend_View_Abstract $view
@@ -182,22 +182,22 @@ class MUtil_Html_TrElement extends MUtil_Html_HtmlElement implements MUtil_Html_
     protected function renderElement(Zend_View_Abstract $view)
     {
         $result = parent::renderElement($view);
-        
+
         if ($this->_onlyWhenChanged) {
             if ($result == $this->_onlyWhenChangedValueStore) {
                 return null;
             }
             $this->_onlyWhenChangedValueStore = $result;
         }
-        
+
         return $result;
     }
     /**
-     * When repeating content using $_repeater you may want to output the content only when it has 
+     * When repeating content using $_repeater you may want to output the content only when it has
      * changed.
-     * 
+     *
      * @see $_repeater
-     * 
+     *
      * @return MUtil_Html_HtmlElement (continuation pattern)
      */
     public function setOnlyWhenChanged($value)
@@ -208,7 +208,7 @@ class MUtil_Html_TrElement extends MUtil_Html_HtmlElement implements MUtil_Html_
 
     /**
      * Static helper function for creation, used by @see MUtil_Html_Creator.
-     * 
+     *
      * @param mixed $arg_array Optional MUtil_Ra::args processed settings
      * @return MUtil_Html_TrElement
      */
