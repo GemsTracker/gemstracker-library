ddb8c220f66d31143f4af938816cb2106bdeba74
Matijs de Jong <mjong@magnafacta.nl>
2012-11-12 16:42:40 +0000

    Updated documentation

--- classes/MUtil/JQuery.php
+++ classes/MUtil/JQuery.php
@@ -3,7 +3,7 @@
 /**
  * Copyright (c) 2011, Erasmus MC
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *    * Redistributions of source code must retain the above copyright
@@ -14,7 +14,7 @@
  *    * Neither the name of Erasmus MC nor the
  *      names of its contributors may be used to endorse or promote products
  *      derived from this software without specific prior written permission.
- *      
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -25,14 +25,31 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Gems
+ * @subpackage JQuery
+ * @author     Matijs de Jong <mjong@magnafacta.nl>
+ * @copyright  Copyright (c) 2011 Erasmus MC
+ * @license    New BSD License
+ * @version    $Id$
  */
 
 /**
- * @package MUtil
+ *
+ * @package    MUtil
+ * @subpackage JQuery
+ * @copyright  Copyright (c) 2011 Erasmus MC
+ * @license    New BSD License
+ * @since      Class available since version 1.0
  */
-
 class MUtil_JQuery extends ZendX_JQuery
 {
+    /**
+     * Returns the jQuery container object assigned to the view helper.
+     * 
+     * @staticvar ZendX_JQuery_View_Helper_JQuery_Container $jquery
+     * @return ZendX_JQuery_View_Helper_JQuery_Container
+     */
     public static function jQuery()
     {
         static $jquery;
@@ -45,7 +62,13 @@ class MUtil_JQuery extends ZendX_JQuery
         return $jquery;
     }
 
-    public static function usesJQuery(Zend_View_Abstract $view) 
+    /**
+     * Check if the view is using JQuery
+     *
+     * @param Zend_View_Abstract $view
+     * @return boolean
+     */
+    public static function usesJQuery(Zend_View_Abstract $view)
     {
         return false !== $view->getPluginLoader('helper')->getPaths('ZendX_JQuery_View_Helper');
     }
