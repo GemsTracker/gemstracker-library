5c52e16aa1e6946d85ccb9e9d3a65c4b40a516d9
Menno Dekker <mennodekker@users.sourceforge.net>
2013-01-16 11:20:38 +0000

    Removed mailer class and moved Gems specific mail handling to Gems_Mail

--- classes/Gems/Mail.php
+++ classes/Gems/Mail.php
@@ -46,17 +46,36 @@
  */
 class Gems_Mail extends MUtil_Mail
 {
+    const MAIL_NO_ENCRYPT = 0;
+    const MAIL_SSL = 1;
+    const MAIL_TLS = 2;
+    
+    /**
+     * @var GemsEscort
+     */
+    public $escort = null;
+    
+    protected static $mailServers = array();
+    
+    public function __construct($charset = null) {
+        parent::__construct($charset);
+        $this->escort = GemsEscort::getInstance();
+    }
+    
     /**
      * Adds To-header and recipient, $email can be an array, or a single string address
      *
      * @param string|array $email
      * @param string $name
-     * @param boolean $bounce When true the e-mail is bounced to the from address
+     * @param boolean $bounce When true the e-mail is bounced to the from address, when omitted bounce is read from project settings
      * @return Zend_Mail Provides fluent interface
      */
-    public function addTo($email, $name = '', $bounce = false)
+    public function addTo($email, $name = '', $bounce = null)
     {
-        if ($bounce) {
+        if (is_null($bounce)) {
+            $bounce = $this->bounceCheck();
+        }
+        if ($bounce === true) {
             $name  = str_replace('@', ' at ', $email);
             $email = $this->getFrom();
 
@@ -66,6 +85,68 @@ class Gems_Mail extends MUtil_Mail
         }
 
         return parent::addTo($email, $name);
+    }
+    
+    /**
+     * Returns true if the "email.bounce" setting exists in the project
+     * configuration and is true
+     * @return boolean
+     */
+    public function bounceCheck()
+    {
+        return $this->escort->project->getEmailBounce();
+    }
+    
+    /**
+     * Returns Zend_Mail_Transport_Abstract when something else than the default mail protocol should be used.
+     *
+     * @staticvar array $mailServers
+     * @param email address $from
+     * @return Zend_Mail_Transport_Abstract or null
+     */
+    public function checkTransport($from)
+    {
+        if (! array_key_exists($from, self::$mailServers)) {
+            $sql = 'SELECT * FROM gems__mail_servers WHERE ? LIKE gms_from ORDER BY LENGTH(gms_from) DESC LIMIT 1';
+
+            // Always set cache, se we know when not to check for this row.
+            $serverData = $this->escort->db->fetchRow($sql, $from);
+
+            // MUtil_Echo::track($serverData);
+
+            if (isset($serverData['gms_server'])) {
+                $options = array();
+                if (isset($serverData['gms_user'], $serverData['gms_password'])) {
+                    $options['auth'] = 'login';
+                    $options['username'] = $serverData['gms_user'];
+                    $options['password'] = $serverData['gms_password'];
+                }
+                if (isset($serverData['gms_port'])) {
+                    $options['port'] = $serverData['gms_port'];
+                }
+                if (isset($serverData['gms_ssl'])) {
+                    switch ($serverData['gms_ssl']) {
+                        case self::MAIL_SSL:
+                            $options['ssl'] = 'ssl';
+                            break;
+
+                        case self::MAIL_TLS:
+                            $options['ssl'] = 'tls';
+                            break;
+
+                        default:
+                            // intentional fall through
+
+                    }
+                }
+
+                self::$mailServers[$from] = new Zend_Mail_Transport_Smtp($serverData['gms_server'], $options);
+            } else {
+                self::$mailServers[$from] = $this->getDefaultTransport();
+            }
+        }
+
+        return self::$mailServers[$from];
     }
 
     /**
@@ -96,5 +177,14 @@ class Gems_Mail extends MUtil_Mail
         $this->setHtmlTemplateFile(APPLICATION_PATH . '/configs/email/' . $style . '.html');
 
         return $this;
+    }
+    
+    public function send($transport = null) {
+        // Before we forward to the Zend_Mail send method, first perfom a bounce check
+        if (is_null($transport)) {
+            $transport = $this->checkTransport($this->getFrom());
+        }
+        
+        parent::send($transport);
     }
 }
