1fbabff604efa1c261ac07a3e34a1fb0a26a60c9
Jasper van Gestel <jvangestel@gmail.com>
2013-10-17 06:47:06 +0000

    small errors in sql syntax

--- configs/db/tables/gems__token2appointments.300.sql
+++ configs/db/tables/gems__token2appointments.300.sql
@@ -1,6 +1,6 @@
 
 
-CREATE TABLE gems__token2appointments {
+CREATE TABLE gems__token2appointments (
         gt2a_id_token           varchar(9) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' not null
                                 references gems__tokens (gto_id_token),
         gt2a_id_appointment     bigint unsigned not null references gems__appointments (gap_id_appointment),
@@ -8,7 +8,7 @@ CREATE TABLE gems__token2appointments {
         gt2a_created            timestamp not null default current_timestamp ,
         gt2a_created_by         bigint unsigned not null,
 
-        PRIMARY KEY (gt2a_id_token),
+        PRIMARY KEY (gt2a_id_token)
     )
     ENGINE=InnoDB
     CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
