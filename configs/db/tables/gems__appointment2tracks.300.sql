6f40f0fad164c53311bf2dfbe9c4bb044a9f799c
Michiel Rook <michiel@touchdownconsulting.nl>
2013-10-21 21:01:27 +0000

    Fix typo

--- configs/db/tables/gems__appointment2tracks.300.sql
+++ configs/db/tables/gems__appointment2tracks.300.sql
@@ -11,7 +11,7 @@ CREATE TABLE gems__appointment2tracks (
         ga2t_created        timestamp not null default '0000-00-00 00:00:00',
         ga2t_created_by     bigint unsigned not null,
 
-        PRIMARY KEY (ga2t_id _respondent_track, ga2t_id_appointment)
+        PRIMARY KEY (ga2t_id_respondent_track, ga2t_id_appointment)
     )
     ENGINE=InnoDB
     CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
