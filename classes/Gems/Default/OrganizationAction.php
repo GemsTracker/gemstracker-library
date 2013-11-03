006cfba820129967e10e93ab7a5c3f9099722a1f
Jasper van Gestel <jvangestel@gmail.com>
2013-11-03 21:51:43 +0000

    more modular template select

--- classes/Gems/Default/OrganizationAction.php
+++ classes/Gems/Default/OrganizationAction.php
@@ -167,8 +167,9 @@ class Gems_Default_OrganizationAction extends Gems_Controller_ModelSnippetAction
             $model->set('gor_name',      'validator', $model->createUniqueValidator('gor_name'));
             $model->set('gor_welcome',   'label', $this->_('Greeting'),  'description', $this->_('For emails and token forward screen.'), 'elementClass', 'Textarea', 'rows', 5);
             $model->set('gor_signature', 'label', $this->_('Signature'), 'description', $this->_('For emails and token forward screen.'), 'elementClass', 'Textarea', 'rows', 5);
-
-            $templates = $this->loader->getMailLoader()->getMailElements()->getAvailableMailTemplates(false, 'staffPassword');
+        
+            $templates = $this->getPasswordTemplates();
+        
             $model->set('gor_create_account_template', 'label', $this->_('Create Account template'), 'multiOptions', $templates);
             $model->set('gor_reset_pass_template', 'label', $this->_('Reset Password template'), 'multiOptions', $templates);
         }
@@ -233,6 +234,15 @@ class Gems_Default_OrganizationAction extends Gems_Controller_ModelSnippetAction
     public function getIndexTitle()
     {
         return $this->_('Participating organizations');
+    }
+
+    /**
+     * Helper function to get the templates for the password mails
+     * @return array
+     */
+    public function getPasswordTemplates()
+    {
+        return $this->loader->getMailLoader()->getMailElements()->getAvailableMailTemplates(false, 'staffPassword');
     }
 
     /**
