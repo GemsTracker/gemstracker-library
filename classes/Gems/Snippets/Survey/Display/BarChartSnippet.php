451e4b33e9ddc880acc5df6d8ca3ea04521114c4
Menno Dekker <mennodekker@users.sourceforge.net>
2014-06-10 07:11:21 +0000

    Some documentation fixes

--- classes/Gems/Snippets/Survey/Display/BarChartSnippet.php
+++ classes/Gems/Snippets/Survey/Display/BarChartSnippet.php
@@ -1,8 +1,41 @@
 <?php
 /**
- * BarChartSnippet
- * 
- * the BarChart snippet provides a single dimension barchart based on one question_code for all surveys using a specific survey_code
+ * Copyright (c) 2014, Erasmus MC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the name of Erasmus MC nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * @package    Gems
+ * @subpackage Snippets\Survey\Display
+ * @author     Menno Dekker <menno.dekker@erasmusmc.nl>
+ * @copyright  Copyright (c) 2014 Erasmus MC
+ * @license    New BSD License
+ * @version    $Id$
+ */
+
+/**
+ * The BarChart snippet provides a single dimension barchart based on one question_code for all surveys using a specific survey_code
  * 
  * Options are:
  *      $min / $max             for min and max values for the chart
@@ -12,7 +45,12 @@
  *      $survey_code
  * 
  *
- * @author Menno Dekker
+ * @package    Gems
+ * @subpackage Snippets\Survey\Display
+ * @author     Menno Dekker <menno.dekker@erasmusmc.nl>
+ * @copyright  Copyright (c) 2014 Erasmus MC
+ * @license    New BSD License
+ * @version    $Id$
  */
 class Gems_Snippets_Survey_Display_BarChartSnippet extends MUtil_Snippets_SnippetAbstract {
     
