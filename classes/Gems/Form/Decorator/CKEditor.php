cd0c9ef79edafb9be818c27b02d265f2e427ff87
Menno Dekker <mennodekker@users.sourceforge.net>
2014-06-16 10:55:36 +0000

    Fixing some checkstyle warnings

--- classes/Gems/Form/Decorator/CKEditor.php
+++ classes/Gems/Form/Decorator/CKEditor.php
@@ -33,64 +33,65 @@
  * @license    New BSD License
  * @version    $Id$
  */
-class Gems_Form_Decorator_CKEditor extends Zend_Form_Decorator_ViewHelper
-{
-	/**
-	 * Default basedir for CKEditor. Can be overwritten through the Decorator options.
-	 * @var string
-	 */
-	private $_basedir = 'gems/ckeditor';
+class Gems_Form_Decorator_CKEditor extends Zend_Form_Decorator_ViewHelper {
 
-	/**
-	 * Element text
-	 * @var string
-	 */
-	private $_format = '<textarea id="%s" name="%s">%s</textarea>';
+    /**
+     * Default basedir for CKEditor. Can be overwritten through the Decorator options.
+     * @var string
+     */
+    private $_basedir = 'gems/ckeditor';
 
-	protected $_options = array();
+    /**
+     * Element text
+     * @var string
+     */
+    private $_format = '<textarea id="%s" name="%s">%s</textarea>';
+
+    protected $_options = array();
 
 	public function __construct($options = null)
 	{
-		//MUtil_Echo::track('test');	
-		// If basepath not set, try a default
+        //MUtil_Echo::track('test');
+        // If basepath not set, try a default
         if ($options) {
-        	if (is_array($options)) {
-        		$this->_options = $options;
-        	} else {
-        		$this->_options[] = $options;
-        	}
-        	if (isset($this->_options['baseDir'])) {
-        		$this->_basedir = $this->_options['baseDir'];
-        	}
+            if (is_array($options)) {
+                $this->_options = $options;
+            } else {
+                $this->_options[] = $options;
+            }
+            if (isset($this->_options['baseDir'])) {
+                $this->_basedir = $this->_options['baseDir'];
+            }
         }
-	}
-	public function render($content)
-	{
-		$element = $this->getElement();
-		$view = $element->getView();
+    }
 
-		$baseUrl = GemsEscort::getInstance()->basepath->getBasePath() . '/';
-		$view->headScript()->appendFile($baseUrl . $this->_basedir . '/' . 'ckeditor.js');
-		
+    public function render($content)
+    {
+        $element = $this->getElement();
+        $view    = $element->getView();
+
+        $baseUrl = GemsEscort::getInstance()->basepath->getBasePath() . '/';
+        $view->headScript()->appendFile($baseUrl . $this->_basedir . '/' . 'ckeditor.js');
+
 
         //MUtil_Echo::track($baseUrl);
-		//MUtil_Echo::track('test');
-		$element = $this->getElement();
-		$name = htmlentities($element->getFullyQualifiedName());
-		$label = htmlentities($element->getLabel());
-		$id = htmlentities($element->getId());
-		$value = htmlentities($element->getValue());
+        //MUtil_Echo::track('test');
+        $element = $this->getElement();
+        $name    = htmlentities($element->getFullyQualifiedName());
+        $label   = htmlentities($element->getLabel());
+        $id      = htmlentities($element->getId());
+        $value   = htmlentities($element->getValue());
 
-		$markup = sprintf($this->_format, $id, $name, $value);
+        $markup = sprintf($this->_format, $id, $name, $value);
 
-		$view = $element->getView();
-		$view->inlineScript()->appendScript("
+        $view = $element->getView();
+        $view->inlineScript()->appendScript("
 			if (typeof CKEditorConfig === 'undefined') {
 				CKEditorConfig = {};
 			}
 			CKEDITOR.replace( '{$id}', CKEditorConfig );
 		");
-		
-		return $markup;
-	}
+
+        return $markup;
+    }
 }
