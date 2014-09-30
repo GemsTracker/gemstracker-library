2cf6dffb26ca1e87ca1af5caff56a2062dd6ceb0
Matijs de Jong <mjong@magnafacta.nl>
2014-09-30 09:53:44 +0000

    Fixed #701: clickjacking can be prevented through x-frame option in project.ini

--- classes/Gems/Project/ProjectSettings.php
+++ classes/Gems/Project/ProjectSettings.php
@@ -112,6 +112,10 @@ class Gems_Project_ProjectSettings extends ArrayObject
         }
 
         $this->offsetSet('multiLocale', $this->offsetExists('locales') && (count($this->offsetGet('locales')) > 1));
+
+        if ($this->offsetExists('x-frame')) {
+            header('X-Frame-Options: ' . $this->offsetGet('x-frame'));
+        }
     }
 
     /**
