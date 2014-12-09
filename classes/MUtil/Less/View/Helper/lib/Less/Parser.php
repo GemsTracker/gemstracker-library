ef5868b1facff5f2164765f5b9d1954087986b71
Menno Dekker <mennodekker@users.sourceforge.net>
2014-12-09 14:01:37 +0000

    Fix for changing environment (to be more precise: precision) when compiling fails resulting in failed unit tests (patch submitted to upstream project)

--- classes/MUtil/Less/View/Helper/lib/Less/Parser.php
+++ classes/MUtil/Less/View/Helper/lib/Less/Parser.php
@@ -174,38 +174,49 @@ class Less_Parser{
 		$locale = setlocale(LC_NUMERIC, 0);
 		setlocale(LC_NUMERIC, "C");
 
- 		$root = new Less_Tree_Ruleset(array(), $this->rules );
-		$root->root = true;
-		$root->firstRoot = true;
+        try {
+            
+            $root = new Less_Tree_Ruleset(array(), $this->rules );
+            $root->root = true;
+            $root->firstRoot = true;
 
 
-		$this->PreVisitors($root);
+            $this->PreVisitors($root);
 
-		self::$has_extends = false;
-		$evaldRoot = $root->compile($this->env);
+            self::$has_extends = false;
+            $evaldRoot = $root->compile($this->env);
 
 
 
-		$this->PostVisitors($evaldRoot);
+            $this->PostVisitors($evaldRoot);
 
-		if( Less_Parser::$options['sourceMap'] ){
-			$generator = new Less_SourceMap_Generator($evaldRoot, Less_Parser::$contentsMap, Less_Parser::$options );
-			// will also save file
-			// FIXME: should happen somewhere else?
-			$css = $generator->generateCSS();
-		}else{
-			$css = $evaldRoot->toCSS();
-		}
+            if( Less_Parser::$options['sourceMap'] ){
+                $generator = new Less_SourceMap_Generator($evaldRoot, Less_Parser::$contentsMap, Less_Parser::$options );
+                // will also save file
+                // FIXME: should happen somewhere else?
+                $css = $generator->generateCSS();
+            }else{
+                $css = $evaldRoot->toCSS();
+            }
 
-		if( Less_Parser::$options['compress'] ){
-			$css = preg_replace('/(^(\s)+)|((\s)+$)/', '', $css);
-		}
+            if( Less_Parser::$options['compress'] ){
+                $css = preg_replace('/(^(\s)+)|((\s)+$)/', '', $css);
+            }
 
+        } catch (Exception $exc) {
+            // Intentional fall-though
+        }
+        
 		//reset php settings
 		@ini_set('precision',$precision);
 		setlocale(LC_NUMERIC, $locale);
+        
+        // Rethrow exception after we handled resetting the environment
+        if (!empty($exc)) {
+            throw $exc;
+        }
 
-		return $css;
+        return $css;
 	}
 
 	/**
