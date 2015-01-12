f77bfa43116f914e563c39ff9c10fa97e9a6e038
Matijs de Jong <mjong@magnafacta.nl>
2015-01-12 18:14:34 +0000

    Helper class for model data arrays

---
+++ classes/MUtil/Ra/RaObject.php
@@ -0,0 +1,222 @@
+<?php
+
+/**
+ * Copyright (c) 2015, Erasmus MC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the name of Erasmus MC nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * @package    MUtil
+ * @subpackage Ra
+ * @author     Matijs de Jong <mjong@magnafacta.nl>
+ * @copyright  Copyright (c) 2015 Erasmus MC
+ * @license    New BSD License
+ * @version    $Id: RaObject.php $
+ */
+
+namespace MUtil\Ra;
+
+/**
+ * TargetInterface version of ArrayObject
+ *
+ * @package    MUtil
+ * @subpackage Ra
+ * @copyright  Copyright (c) 2015 Erasmus MC
+ * @license    New BSD License
+ * @since      Class available since MUtil version 1.4 11-jan-2015 15:01:42
+ */
+class RaObject extends \ArrayObject implements \MUtil_Registry_TargetInterface
+{
+    /**
+     * Variables e.g. set through the source that should not be serialized
+     *
+     * @var array Arrayof object names
+     */
+    protected $_varsNotSerialized = array();
+
+    /**
+     *
+     * @var \Zend_Translate
+     */
+    protected $translate;
+
+    /**
+     *
+     * @var \Zend_Translate_Adapter
+     */
+    protected $translateAdapter;
+
+    /**
+     * Copy from Zend_Translate_Adapter
+     *
+     * Translates the given string
+     * returns the translation
+     *
+     * @param  string             $text   Translation string
+     * @param  string|Zend_Locale $locale (optional) Locale/Language to use, identical with locale
+     *                                    identifier, @see Zend_Locale for more information
+     * @return string
+     */
+    public function _($text, $locale = null)
+    {
+        return $this->translateAdapter->_($text, $locale);
+    }
+
+    /**
+     * Called after the check that all required registry values
+     * have been set correctly has run.
+     *
+     * This function is no needed if the classes are setup correctly
+     *
+     * @return void
+     */
+    public function afterRegistry()
+    {
+        $this->initTranslateable();
+    }
+
+    /**
+     * Allows the loader to set resources.
+     *
+     * @param string $name Name of resource to set
+     * @param mixed $resource The resource.
+     * @return boolean True if $resource was OK
+     */
+    public function answerRegistryRequest($name, $resource)
+    {
+        if (\MUtil_Registry_Source::$verbose) {
+            \MUtil_Echo::r('Resource set: ' . get_class($this) . '->' . __FUNCTION__ .
+                    '("' . $name . '", ' .
+                    (is_object($resource) ? get_class($resource) : gettype($resource)) . ')');
+        }
+        $this->_varsNotSerialized[] = $name;
+        $this->$name = $resource;
+
+        return true;
+    }
+
+    /**
+     * Should be called after answering the request to allow the Target
+     * to check if all required registry values have been set correctly.
+     *
+     * @return boolean False if required values are missing.
+     */
+    public function checkRegistryRequestsAnswers()
+    {
+        return true;
+    }
+
+    /**
+     * Filters the names that should not be requested.
+     *
+     * Can be overriden.
+     *
+     * @param string $name
+     * @return boolean
+     */
+    protected function filterRequestNames($name)
+    {
+        return '_' !== $name[0];
+    }
+
+    /**
+     * Allows the loader to know the resources to set.
+     *
+     * Returns those object variables defined by the subclass but not at the level of this definition.
+     *
+     * Can be overruled.
+     *
+     * @return array of string names
+     */
+    public function getRegistryRequests()
+    {
+        return array_filter(array_keys(get_object_vars($this)), array($this, 'filterRequestNames'));
+    }
+
+    /**
+     * Function that checks the setup of this class/traight
+     *
+     * This function is not needed if the variables have been defined correctly in the
+     * source for this object and theose variables have been applied.
+     *
+     * return @void
+     */
+    protected function initTranslateable()
+    {
+        if ($this->translateAdapter instanceof \Zend_Translate_Adapter) {
+            // OK
+            return;
+        }
+
+        if ($this->translate instanceof \Zend_Translate) {
+            // Just one step
+            $this->translateAdapter = $this->translate->getAdapter();
+            return;
+        }
+
+        if ($this->translate instanceof \Zend_Translate_Adapter) {
+            // It does happen and if it is all we have
+            $this->translateAdapter = $this->translate;
+            return;
+        }
+
+        // Make sure there always is an adapter, even if it is fake.
+        $this->translateAdapter = new \MUtil_Translate_Adapter_Potemkin();
+    }
+
+    /**
+     * Copy from Zend_Translate_Adapter
+     *
+     * Translates the given string using plural notations
+     * Returns the translated string
+     *
+     * @see Zend_Locale
+     * @param  string             $singular Singular translation string
+     * @param  string             $plural   Plural translation string
+     * @param  integer            $number   Number for detecting the correct plural
+     * @param  string|Zend_Locale $locale   (Optional) Locale/Language to use, identical with
+     *                                      locale identifier, @see Zend_Locale for more information
+     * @return string
+     */
+    public function plural($singular, $plural, $number, $locale = null)
+    {
+        $args = func_get_args();
+        return call_user_func_array(array($this->translateAdapter, 'plural'), $args);
+    }
+
+    /**
+     * Serialize the object
+     *
+     * @return string The serialized representation of this object
+     */
+    public function serialize()
+    {
+        $output = get_object_vars($this);
+        foreach ($this->_varsNotSerialized as $name) {
+            unset($output[$name]);
+        }
+
+        return serialize($output);
+    }
+}
