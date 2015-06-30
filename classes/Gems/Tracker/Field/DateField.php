eb25d096c1e36faffb06a940d99489945f9dccc7
Matijs de Jong <mjong@magnafacta.nl>
2015-06-30 15:42:52 +0000

    Do not return Zend_Db_Expr objects

--- classes/Gems/Tracker/Field/DateField.php
+++ classes/Gems/Tracker/Field/DateField.php
@@ -94,7 +94,7 @@ class DateField extends FieldAbstract
         if ((null === $currentValue) ||
                 ($currentValue instanceof \Zend_Db_Expr) ||
                 \MUtil_String::startsWith($currentValue, 'current_', true)) {
-            return $currentValue;
+            return null;
         }
 
         if ($currentValue instanceof \Zend_Date) {
