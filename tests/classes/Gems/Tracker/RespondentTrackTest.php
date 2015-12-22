e6904c47288f44a666b49d019392d7f48957e76b
Menno Dekker <menno.dekker@erasmusmc.nl>
2015-12-22 15:03:59 +0100

    Improved unit test for RespondentTrack. No more stubs, but using the database class (also exposing some unexpected behaviour)

--- tests/classes/Gems/Tracker/RespondentTrackTest.php
+++ tests/classes/Gems/Tracker/RespondentTrackTest.php
@@ -1,22 +1,5 @@
 <?php
-/**
- * MD 20151125 - This is a really messy test class to expose a problem and test the fix. Use and / or extend with care. 
- */
-
-class containerStub {
-    public function getEvents() {
-        return array();
-    }
-}       
-
-//include_once 'Gems/Tracker/Engine/FieldsDefinition.php';
-//include_once 'Gems/Tracker/Field/FieldInterface.php';
-//include_once 'Gems/Tracker/Field/FieldAbstract.php';
-//include_once 'Gems/Tracker/Field/TextField.php';
-
-//use Gems\Tracker\Engine\FieldsDefinition as FieldsDefinitionAlias;
-
-class Gems_Tracker_RespondentTrackTest extends PHPUnit_Framework_TestCase
+class Gems_Tracker_RespondentTrackTest extends Gems_Test_DbTestAbstract
 {
     /**
      * @var Gems_Tracker_TrackerInterface
@@ -27,73 +10,78 @@ class Gems_Tracker_RespondentTrackTest extends PHPUnit_Framework_TestCase
      * @var Gems_Tracker_Engine_TrackEngineInterface
      */
     protected $engine;
+    
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        // \Zend_Application: loads the autoloader
+        require_once 'Zend/Application.php';
 
-    public function __construct() {
-        parent::__construct();
+        // Create application, bootstrap, and run
+        $application = new \Zend_Application(
+            APPLICATION_ENV,
+            GEMS_ROOT_DIR . '/configs/application.example.ini'
+        );
+
+        $this->bootstrap = $application;
         
-        $fieldsData = array('f__1' => 'test');        
-        $fieldsArray = array('f__1' => 'codename');
-        $s = new \Gems\Tracker\Engine\FieldsDefinition(1);
-        $fieldsDefStub = $this->getMock('Gems\\Tracker\\Engine\\FieldsDefinition',
-             array('getFieldCodes', 'saveFields'),
-             array(1),
-             'MockFieldsDef',
-              true);
+        include_once GEMS_TEST_DIR . '/library/Gems/Cookies.php';   // Dirty fix for cookie problem during tests
         
-        $fieldsDefStub->expects($this->any())
-            ->method('getFieldCodes')
-            ->will($this->returnValue($fieldsArray));
+        parent::setUp();
+
+        $this->bootstrap->bootstrap('db');
+        $this->bootstrap->getBootstrap()->getContainer()->db = $this->db;
         
-        $fieldsDefStub->expects($this->any())
-            ->method('saveFields')
-            ->will($this->returnValue(1));
+        $this->bootstrap->bootstrap();
         
-        $fieldsDefStub->answerRegistryRequest('_fields', array('f__1' => new Gems\Tracker\Field\TextField(1, 'f__1',
-            array(
-                'sub' => 'f',
-                'gtf_id_field' => 1,
-                'gtf_field_name' => 'Name',
-                'gtf_field_code' => 'codename'
-            )
-            )));
-        $fieldsDefStub->answerRegistryRequest('exists', true);
-
-
-        // Create a stub for the tracker to return an engine
-        $trackData = array('gtr_id_track' => 1);
-        $engineStub = $this->getMock('Gems_Tracker_Engine_AnyStepEngine',
-                     array('getFieldsData'),
-                     array($trackData),
-                     'MockEngine',
-                      true);
-
-        $engineStub->expects($this->any())
-            ->method('getFieldsData')
-            ->will($this->returnValue($fieldsData));
+        \Zend_Registry::set('db', $this->db);
+        \Zend_Db_Table::setDefaultAdapter($this->db);
         
-        $engineStub->answerRegistryRequest('_fieldsDefinition', $fieldsDefStub);
-
-        // Create a stub for the tracker to return an engine
+        $this->loader = $this->bootstrap->getBootstrap()->getResource('loader');
         
-        $container = new stdClass();
-        $container->loader = new containerStub();
-        $trackerStub = $this->getMock('Gems_Tracker',
-                     array('getTrackEngine'),
-                     array($container, array()),
-                     'MockTracker',
-                     true);
+        $this->tracker = $this->loader->getTracker();
+        
+         // Now set some defaults
+        $dateFormOptions['dateFormat']   = 'dd-MM-yyyy';
+        $datetimeFormOptions['dateFormat']   = 'dd-MM-yyyy HH:mm';
+        $timeFormOptions['dateFormat']   = 'HH:mm';
 
-        $trackerStub->expects($this->any())
-                ->method('getTrackEngine')
-                ->will($this->returnValue($engineStub));
-
-        $this->tracker = $trackerStub;
+        MUtil_Model_Bridge_FormBridge::setFixedOptions(array(
+            'date'     => $dateFormOptions,
+            'datetime' => $datetimeFormOptions,
+            'time'     => $timeFormOptions,
+            ));
     }
     
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+    
+    /**
+     * Returns the test dataset.
+     *
+     * @return PHPUnit_Extensions_Database_DataSet_IDataSet
+     */
+    protected function getDataSet()
+    {
+        //Dataset TokenTest.xml has the minimal data we need to perform our tests
+        $classFile =  str_replace('.php', '.xml', __FILE__);
+        return $this->createFlatXMLDataSet($classFile);
+    }
+   
     public function testGetFieldCodes()
     {
         $expected = array(
-            'f__1' => 'codename'
+            'f__1' => 'code',
+            'f__2' => 'datecode'
             );
         
         $this->assertEquals($expected, $this->tracker->getTrackEngine(1)->getFields());
@@ -104,22 +92,64 @@ class Gems_Tracker_RespondentTrackTest extends PHPUnit_Framework_TestCase
         $trackData = array('gr2t_id_respondent_track' => 1, 'gr2t_id_track' => 1);
         $respondentTrack = new Gems_Tracker_RespondentTrack($trackData);
         $respondentTrack->answerRegistryRequest('tracker', $this->tracker);
+        $date = new MUtil_Date('2010-10-08', 'yyyy-MM-dd');
         $expected = array(
             'f__1' => 'test',
-            'codename' => 'test'
+            'code' => 'test',
+            'f__2' => $date,
+            'datecode' => $date
             );
         $this->assertEquals($expected, $respondentTrack->getFieldData());
     }
     
+    /**
+     * Test if settings fields via code works
+     */
     public function testSetFields()
     {
         $trackData = array('gr2t_id_respondent_track' => 1, 'gr2t_id_track' => 1);
         $respondentTrack = new Gems_Tracker_RespondentTrack($trackData);
         $respondentTrack->answerRegistryRequest('tracker', $this->tracker);
         $expected = $respondentTrack->getFieldData();
-        foreach($expected as $key => &$value) {
-            $value = 'newvalue';
-        }
-        $this->assertEquals($expected, $respondentTrack->setFieldData(array('codename' => 'newvalue')));
+        $date = new MUtil_Date('2010-11-09', 'yyyy-MM-dd');
+        $expected = array(
+            'f__1' => 'newvalue',
+            'code' => 'newvalue',
+            'f__2' => $date,
+            'datecode' => $date
+            );
+        $this->assertEquals($expected, $respondentTrack->setFieldData(array('code' => 'newvalue', 'datecode' => $date)));
+    }
+    
+    /**
+     * When saving a date by using a string, it should work too
+     */
+    public function testSetDateFields()
+    {
+        $trackData = array('gr2t_id_respondent_track' => 1, 'gr2t_id_track' => 1);
+        $respondentTrack = new Gems_Tracker_RespondentTrack($trackData);
+        $respondentTrack->answerRegistryRequest('tracker', $this->tracker);
+        $expected = $respondentTrack->getFieldData();
+        $date = new MUtil_Date('2010-11-09', 'yyyy-MM-dd');
+        $expected = array(
+            'f__1' => 'newvalue',
+            'code' => 'newvalue',
+            'f__2' => $date,
+            'datecode' => $date
+            );
+        $this->assertEquals($expected, $respondentTrack->setFieldData(array('code' => 'newvalue', 'datecode' => $date->toString('yyy-MM-dd'))));
+    }
+    
+    /**
+     * When only providing one or two fields, the others should not get nulled
+     */
+    public function testSetFieldsPartial()
+    {
+        $trackData = array('gr2t_id_respondent_track' => 1, 'gr2t_id_track' => 1);
+        $respondentTrack = new Gems_Tracker_RespondentTrack($trackData);
+        $respondentTrack->answerRegistryRequest('tracker', $this->tracker);
+        $expected = $respondentTrack->getFieldData();
+        $expected['f__1'] = $expected['code'] = 'newvalue';
+        $this->assertEquals($expected, $respondentTrack->setFieldData(array('code' => 'newvalue')));
     }
 }
