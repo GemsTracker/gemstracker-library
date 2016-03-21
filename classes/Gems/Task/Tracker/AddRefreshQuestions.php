4fa19e72cd5ae94cd5d8ceb1c5b9e4d2ecf03aa3
Menno Dekker <menno.dekker@erasmusmc.nl>
2016-03-21 15:13:31 +0100

    Bugfix: Made survey sync with response database more robust

--- classes/Gems/Task/Tracker/AddRefreshQuestions.php
+++ classes/Gems/Task/Tracker/AddRefreshQuestions.php
@@ -46,10 +46,17 @@
 class Gems_Task_Tracker_AddRefreshQuestions extends \MUtil_Task_TaskAbstract
 {
     /**
+     * The Gems DB
+     *
+     * @var \Zend_Db_Adapter_Abstract
+     */
+    protected $db;
+
+    /**
      * @var \Gems_Loader
      */
     protected $loader;
-    
+
     /**
      * @var Gems_Project_ProjectSettings
      */
@@ -83,16 +90,16 @@ class Gems_Task_Tracker_AddRefreshQuestions extends \MUtil_Task_TaskAbstract
                 $batch->addTask('Tracker_RefreshQuestion', $surveyId, $name, $order);
             }
         }
-        
+
         // If we have a response database, create a view on the answers
         if ($this->project->hasResponseDatabase()) {
-            $this->replaceCreateView($this->getViewName($survey), $answerModel);            
+            $this->replaceCreateView($survey, $answerModel);
         }
     }
-    
+
     /**
      * Get a name for the view
-     * 
+     *
      * @param \Gems_Tracker_Survey $survey
      * @return string
      */
@@ -100,14 +107,15 @@ class Gems_Task_Tracker_AddRefreshQuestions extends \MUtil_Task_TaskAbstract
     {
         return 'T' . $survey->getSurveyId();
     }
-    
+
     /**
      * Handles creating or replacing the view for this survey
-     * 
-     * @param string                     $viewName
+     *
+     * @param \Gems_Tracker_Survey       $viewName
      * @param \MUtil_Model_ModelAbstract $answerModel
      */
-    protected function replaceCreateView($viewName, \MUtil_Model_ModelAbstract $answerModel) {
+    protected function replaceCreateView(\Gems_Tracker_Survey $survey, \MUtil_Model_ModelAbstract $answerModel) {
+        $viewName = $this->getViewName($survey);
         $responseDb = $this->project->getResponseDatabase();
         $fieldSql   = '';
 
@@ -115,17 +123,25 @@ class Gems_Task_Tracker_AddRefreshQuestions extends \MUtil_Task_TaskAbstract
             if (true === $answerModel->get($name, 'survey_question') && // It should be a question
                     !in_array($name, array('submitdate', 'startdate', 'datestamp')) && // Leave out meta info
                     !$answerModel->is($name, 'type', \MUtil_Model::TYPE_NOVALUE)) {         // Only real answers
-                $fieldSql .= ',MAX(IF(gdr_answer_id = ' . $responseDb->quote($name) . ', gdr_response, NULL)) AS ' . $responseDb->quote($name);
+                    $fieldSql .= ',MAX(IF(gdr_answer_id = ' . $responseDb->quote($name) . ', gdr_response, NULL)) AS ' . $responseDb->quoteIdentifier($name);
             }
         }
 
         if ($fieldSql > '') {
-            $createViewSql = 'CREATE OR REPLACE VIEW ' . $responseDb->quote($viewName) . ' AS SELECT gdr_id_token';
+            $dbConfig = $this->db->getConfig();
+            $tokenTable = $this->db->quoteIdentifier($dbConfig[ 'dbname'] . '.gems__tokens');
+            $createViewSql = 'CREATE OR REPLACE VIEW ' . $responseDb->quoteIdentifier($viewName) . ' AS SELECT gdr_id_token';
             $createViewSql .= $fieldSql;
-            $createViewSql .= "FROM gemsdata__responses join gems__tokens on (gto_id_token=gdr_id_token and gto_id_survey=" . $surveyId . ") GROUP BY gdr_id_token;";
-
-            $responseDb->query($createViewSql)->execute();
+            $createViewSql .= "FROM gemsdata__responses join " . $tokenTable . " on (gto_id_token=gdr_id_token and gto_id_survey=" . $survey->getSurveyId() . ") GROUP BY gdr_id_token;";
+            try {
+                $responseDb->query($createViewSql)->execute();
+            } catch (Exception $exc) {
+                $responseConfig = $responseDb->getConfig();
+                $dbUser = $this->db->quoteIdentifier($responseConfig['username']) . '@' . $this->db->quoteIdentifier($responseConfig['host']);
+                $statement = "GRANT SELECT ON  " . $tokenTable . " TO " . $dbUser;
+                $this->getBatch()->addMessage(sprintf($this->_("Creating view failed, try adding rights using the following statement: %s"), $statement));
+            }
         }
     }
-    
+
 }
