42ba132339377c683477c1449f989796e533dfa3
Menno Dekker <menno.dekker@erasmusmc.nl>
2016-03-24 16:02:32 +0100

    Added mock

--- tests/classes/Gems/Tracker/Source/LimeSurvey1m9FieldMapTest.php
+++ tests/classes/Gems/Tracker/Source/LimeSurvey1m9FieldMapTest.php
@@ -5,13 +5,43 @@ class Gems_Tracker_Source_LimeSurvey1m9FieldMapTest extends \Gems_Test_DbTestAbs
 
     protected function setUp()
     {
+        // \Zend_Application: loads the autoloader
+        require_once 'Zend/Application.php';
+
+        // Create application, bootstrap, and run
+        $application = new \Zend_Application(
+            APPLICATION_ENV,
+            GEMS_ROOT_DIR . '/configs/application.example.ini'
+        );
+
+        $this->bootstrap = $application;
+
+        include_once GEMS_TEST_DIR . '/library/Gems/Cookies.php';   // Dirty fix for cookie problem during tests
+        
         parent::setUp();
+        
+        $this->bootstrap->bootstrap('db');
+        $this->bootstrap->getBootstrap()->getContainer()->db = $this->db;
+
+        $this->bootstrap->bootstrap();
+
+        \Zend_Registry::set('db', $this->db);
+        \Zend_Db_Table::setDefaultAdapter($this->db);
 
         $sourceSurveyId = 1;
 		$language = 'en';
 		$lsDb = $this->db;
 		$translate = $this->getTranslate();
 		$tablePrefix = '';
+        
+        // Mock maken voor lsDb
+        $outputArray = array(array());
+        $lsDb = $this->getMockBuilder('Zend_Db_Adapter_Pdo_Sqlite')
+                      ->disableOriginalConstructor()
+                      ->getMock();
+        $lsDb->expects($this->any())
+              ->method('fetchAll')
+              ->will($this->returnValue($outputArray));
 
 		$this->fieldmap = new \Gems_Tracker_Source_LimeSurvey1m9FieldMap($sourceSurveyId, $language, $lsDb, $translate, $tablePrefix);
     }
@@ -37,7 +67,7 @@ class Gems_Tracker_Source_LimeSurvey1m9FieldMapTest extends \Gems_Test_DbTestAbs
         return array($path . 'sqllite/create-lite-ls.sql');
     }
 
-	protected function getTranslate()
+    protected function getTranslate()
 	{
 		$english = array(
         	'test' => 'test',
