1400aee9ffc0f7759e45b652f3292d00b716a12c
Jasper van Gestel <jvangestel@gmail.com>
2016-03-31 07:50:58 +0200

    Fixed #826: LimesurveyFielMap has $row being redefined 2 times. Due to the lacking of the sgq value, Multiopion questions gave errors. the $row = $row[i]; redefines might not be needed?

--- classes/Gems/Tracker/Source/LimeSurvey1m9FieldMap.php
+++ classes/Gems/Tracker/Source/LimeSurvey1m9FieldMap.php
@@ -240,7 +240,6 @@ class Gems_Tracker_Source_LimeSurvey1m9FieldMap
             for ($i = 0; $i < $rowscount; $i++) {
                 $row = $rows[$i];
                 $row['sgq'] = $row['sid'] . 'X' . $row['gid'] . 'X' . $row['qid'];
-
                 $other = ($row['other'] == 'Y');
 
                 switch ($row['type']) {
@@ -279,6 +278,7 @@ class Gems_Tracker_Source_LimeSurvey1m9FieldMap
                     case 'P':    //Multiple options with other and comment
                         do {
                             $row = $rows[$i];
+                            $row['sgq'] = $row['sid'] . 'X' . $row['gid'] . 'X' . $row['qid'];
                             $row['sgq'] .= $row['sq_title'];
                             if ($rows[$i]['type'] === 'O') {    // List, only one answer don't add _
                                 $row['code'] = $row['title'];
@@ -300,6 +300,7 @@ class Gems_Tracker_Source_LimeSurvey1m9FieldMap
                         $i--;
                         if ($other) {
                             $row = $rows[$i];
+                            $row['sgq'] = $row['sid'] . 'X' . $row['gid'] . 'X' . $row['qid'];
                             $row['sgq'] .= 'other';
                             $row['sq_title'] = 'other';
                             $row['code'] = $row['title'] . '_' . $row['sq_title'];
