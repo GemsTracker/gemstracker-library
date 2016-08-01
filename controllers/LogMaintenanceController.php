291e79540f07ee85cffbed25ce5c8d53f032d928
Matijs de Jong <mjong@magnafacta.nl>
2016-08-01 11:22:18 +0200

    To much comment removed from files

--- controllers/LogMaintenanceController.php
+++ controllers/LogMaintenanceController.php
@@ -2,7 +2,23 @@
 
 
 /**
+ *
+ * @package    Gems
+ * @subpackage Default
+ * @author     Matijs de Jong <mjong@magnafacta.nl>
+ * @copyright  Copyright (c) 2013 Erasmus MC
+ * @license    New BSD License
+ */
 
-class LogMaintenanceController extends Gems_Default_LogMaintenanceAction 
+/**
+ * Stub for overruling at project level.
+ *
+ * @package    Gems
+ * @subpackage Default
+ * @copyright  Copyright (c) 2013 Erasmus MC
+ * @license    New BSD License
+ * @since      Class available since version 1.6.2
+ */
+class LogMaintenanceController extends Gems_Default_LogMaintenanceAction
 {
 }
--- controllers/SurveyMaintenanceController.php
+++ controllers/SurveyMaintenanceController.php
@@ -2,7 +2,23 @@
 
 
 /**
+ *
+ * @package    Gems
+ * @subpackage Default
+ * @author     Matijs de Jong <mjong@magnafacta.nl>
+ * @copyright  Copyright (c) 2013 Erasmus MC
+ * @license    New BSD License
+ */
 
+/**
+ * Stub for overruling at project level.
+ *
+ * @package    Gems
+ * @subpackage Default
+ * @copyright  Copyright (c) 2013 Erasmus MC
+ * @license    New BSD License
+ * @since      Class available since version 1.0
+ */
 class SurveyMaintenanceController extends Gems_Default_SurveyMaintenanceAction
 {
 }
