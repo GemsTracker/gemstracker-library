15e906b7466cb78593f19e6c4082282567a6306a
Jasper van Gestel <jvangestel@gmail.com>
2017-07-06 16:56:54 +0200

    Added a survey update feature where you can compare an old and new survey and transfer answers and tokens to the new version

---
+++ controllers/UpdateSurveyController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *
+ * @package    Gems
+ * @subpackage Default
+ * @author     Jasper van Gestel <jvangestel@gmail.com>
+ * @copyright  Copyright (c) 2017 Erasmus MC
+ * @license    New BSD License
+ * @version    
+ */
+
+/**
+ * Stub for overruling at project level.
+ *
+ * @package    Gems
+ * @subpackage Default
+ * @copyright  Copyright (c) 2017 Erasmus MC
+ * @license    New BSD License
+ * @since      Class available since version 1.8.2
+ */
+class UpdateSurveyController extends \Gems_Default_UpdateSurveyAction
+{
+}
