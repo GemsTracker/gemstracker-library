GemsTracker/gemstracker-library

commit 69651366bc1d249fa23e745c536e52139183072e

Changed to sqlite compatible function for unit tests

classes/Gems/Util/TrackData.php
@@ -179,9 +179,9 @@ class Gems_Util_TrackData extends UtilAbstract
 
         $select = 'SELECT gsu_id_survey,
             	CONCAT(
-            		LEFT(CONCAT_WS(
+            		SUBSTR(CONCAT_WS(
             			" - ", gsu_survey_name, CASE WHEN LENGTH(TRIM(gsu_survey_description)) = 0 THEN NULL ELSE gsu_survey_description END
-            		), 50),
+            		), 0, 50),
         			CASE WHEN gsu_active = 1 THEN " (' . $this->translate->_('Active') . ')" ELSE " (' . $this->translate->_('Inactive') . ')" END
     			)
             	FROM gems__surveys ORDER BY gsu_survey_name';

commit 7c1267ccfafb714215f0a416789fc7910ce85204

Fixed error introduced in 69651366bc1d249fa23e745c536e52139183072e Dev: substr in sql starts with 1 instead of 0

classes/Gems/Util/TrackData.php
@@ -181,7 +181,7 @@ class Gems_Util_TrackData extends UtilAbstract
             	CONCAT(
             		SUBSTR(CONCAT_WS(
             			" - ", gsu_survey_name, CASE WHEN LENGTH(TRIM(gsu_survey_description)) = 0 THEN NULL ELSE gsu_survey_description END
-            		), 0, 50),
+            		), 1, 50),
         			CASE WHEN gsu_active = 1 THEN " (' . $this->translate->_('Active') . ')" ELSE " (' . $this->translate->_('Inactive') . ')" END
     			)
             	FROM gems__surveys ORDER BY gsu_survey_name';