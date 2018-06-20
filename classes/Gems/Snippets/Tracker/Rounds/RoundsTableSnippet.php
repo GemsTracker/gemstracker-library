e8f71ac00da4c8dd54eb3aa0e88f173129e478ea
Menno Dekker <menno.dekker@erasmusmc.nl>
2018-06-20 16:48:23 +0200

    FIxes for conditions

--- classes/Gems/Snippets/Tracker/Rounds/RoundsTableSnippet.php
+++ classes/Gems/Snippets/Tracker/Rounds/RoundsTableSnippet.php
@@ -93,6 +93,22 @@ class RoundsTableSnippet extends \Gems_Snippets_ModelTableSnippetAbstract
             '',
             'inactive'
         ));
+        
+        // Add link to survey-edit
+        $menuItems = $this->findMenuItems('survey-maintenance', 'edit');
+        if ($menuItems) {
+            $menuItem = reset($menuItems);
+            if ($menuItem instanceof \Gems_Menu_SubMenuItem) {
+                $href = $menuItem->toHRefAttribute($bridge);
+
+                if ($href) {
+                    $aElem = new \MUtil_Html_AElement($href);
+                    $aElem->setOnEmpty('');
+
+                    $model->set('gro_id_survey', 'itemDisplay', $aElem);
+                }
+            }
+        }
 
         parent::addBrowseTableColumns($bridge, $model);
     }
@@ -162,7 +178,7 @@ class RoundsTableSnippet extends \Gems_Snippets_ModelTableSnippetAbstract
         // $this->model->addTable('gems__surveys', array('gro_id_survey' => 'gsu_id_survey'));
 
         // $this->model->set('gsu_survey_name', $this->model->get('gro_id_survey'));
-
+        
         return $this->model;
     }
 }
