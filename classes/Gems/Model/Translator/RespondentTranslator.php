30e2e8cfaa9a9c5396fab48a64b28d0330e7da15
Menno Dekker <menno.dekker@erasmusmc.nl>
2018-08-23 17:22:05 +0200

    Fixed #349

--- classes/Gems/Model/Translator/RespondentTranslator.php
+++ classes/Gems/Model/Translator/RespondentTranslator.php
@@ -105,7 +105,7 @@ class Gems_Model_Translator_RespondentTranslator extends \Gems_Model_Translator_
             }
         }
 
-        if (!isset($row['gr2o_email'])) {
+        if (empty($row['gr2o_email'])) {
             $row['calc_email'] = 1;
         }
 
