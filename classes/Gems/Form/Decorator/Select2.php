86b407c3d569e95a390603c3ae5dba747c9b8a4a
Jasper van Gestel <jvangestel@gmail.com>
2020-05-22 15:13:41 +0200

    Added a decorator for select2 elements, directing to the correct gems asset directory

---
+++ classes/Gems/Form/Decorator/Select2.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Gems_Form_Decorator_Select2 extends \MUtil_Form_Decorator_Select2
+{
+    /**
+     * Default basedir for js files. Can be overwritten through the Decorator options.
+     * @var string
+     */
+    protected $_cssPath = 'gems/css';
+
+    /**
+     * Default basedir for js files. Can be overwritten through the Decorator options.
+     * @var string
+     */
+    protected $_jsPath = 'gems/js';
+}
