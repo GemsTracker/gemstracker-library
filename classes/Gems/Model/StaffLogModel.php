5c932352788f79636c4669621e5fa9819038ed97
Jasper van Gestel <jvangestel@gmail.com>
2021-06-05 18:07:44 +0200

    Fixed staff log action not loading after switch to hiddenOrganizationModel

---
+++ classes/Gems/Model/StaffLogModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Gems\Model;
+
+class StaffLogModel extends LogModel
+{
+    /**
+     * Create a model for the log
+     */
+    public function __construct()
+    {
+        \Gems_Model_HiddenOrganizationModel::__construct('StaffLog', 'gems__staff', 'gsf', true);
+        $this->addTable('gems__log_activity', ['gla_by' => 'gsf_id_user'], 'gla', true);
+        $this->addTable('gems__log_setup', ['gla_action' => 'gls_id_action'])
+            ->addLeftTable('gems__respondents', ['gla_respondent_id' => 'grs_id_user']);
+    }
+}
