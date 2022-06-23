be3bd3f2434e8090c27a2ae8333960f3e5a24860
Jasper van Gestel <jvangestel@gmail.com>
2022-06-23 20:48:50 +0200

    Add LegacyCurrentUserMiddleware now adding a temp currentUserId to model

---
+++ classes/Gems/Middleware/LegacyCurrentUserMiddleware.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Gems\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class LegacyCurrentUserMiddleware implements MiddlewareInterface
+{
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // Change to actual current user id!
+        \Gems_Model::setCurrentUserId(1);
+        return $handler->handle($request);
+    }
+}
