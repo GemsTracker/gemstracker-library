0df1ed929c3d25c42d60e3b806cca58e54b67bce
Jasper van Gestel <jvangestel@gmail.com>
2022-07-19 06:04:42 +0200

    Add messenger bus

---
+++ classes/Gems/Messenger/MessengerFactory.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Gems\Messenger;
+
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Messenger\Handler\HandlersLocator;
+use Symfony\Component\Messenger\MessageBus;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Middleware\AddBusNameStampMiddleware;
+use Symfony\Component\Messenger\Middleware\DispatchAfterCurrentBusMiddleware;
+use Symfony\Component\Messenger\Middleware\FailedMessageProcessingMiddleware;
+use Symfony\Component\Messenger\Middleware\HandleMessageMiddleware;
+use Symfony\Component\Messenger\Middleware\RejectRedeliveredMessageMiddleware;
+use Symfony\Component\Messenger\Middleware\SendMessageMiddleware;
+use Symfony\Component\Messenger\Transport\Sender\SendersLocator;
+
+class MessengerFactory implements FactoryInterface
+{
+
+    private string $busName;
+
+    public function __construct(string $busName = 'messenger.bus.default')
+    {
+        $this->busName = $busName;
+    }
+    
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null): MessageBusInterface
+    {
+        $config = $container->get('config');
+        $customMiddleware = [];
+
+        if (!isset($config['messenger'], $config['messenger']['buses'], $config['messenger']['buses'][$this->busName])) {
+            throw new \Exception('No message bus config found');
+        }
+        if (isset($config['messenger']['buses'][$this->busName]['middleware'])) {
+            $customMiddleware = $config['messenger']['buses'][$this->busName]['middleware'];
+        }
+
+        $eventDispatcher = $container->get(EventDispatcherInterface::class);
+
+
+        $sendMessageLocator = $this->getSendMessageLocator($config['messenger']['buses'][$this->busName], $container);
+        $sendMessageMiddleware = new SendMessageMiddleware($sendMessageLocator, $eventDispatcher);
+
+        $allowNoHandlers = false;
+        if (isset($config['messenger']['buses'][$this->busName]['allowNoHandlers'])) {
+            $allowNoHandlers = $config['messenger']['buses'][$this->busName]['allowNoHandlers'];
+        }
+
+        $handleMessageLocator = $this->getHandleMessageLocator($config['messenger']['buses'][$this->busName]);
+        $handleMessageMiddleware = new HandleMessageMiddleware($handleMessageLocator, $allowNoHandlers);
+
+        if ($container->has('messengerLogger')) {
+            $logger = $container->get('messengerLogger');
+            $sendMessageMiddleware->setLogger($logger);
+            $handleMessageMiddleware->setLogger($logger);
+        }
+
+        $middleware = [
+            new AddBusNameStampMiddleware($this->busName),
+            new RejectRedeliveredMessageMiddleware(),
+            new DispatchAfterCurrentBusMiddleware(),
+            new FailedMessageProcessingMiddleware(),
+            ...$customMiddleware,
+            $sendMessageMiddleware,
+            $handleMessageMiddleware,
+        ];
+
+        return new MessageBus($middleware);
+    }
+
+    protected function getHandleMessageLocator(array $config): HandlersLocator
+    {
+        if (!isset($config['handlers'])) {
+            throw new \Exception(sprintf('No handlers found for bus %s', $this->busName));
+        }
+        return new HandlersLocator($config['handlers']);
+    }
+
+    protected function getSendMessageLocator(array $config, ContainerInterface $container): SendersLocator
+    {
+        if (!isset($config['routes'])) {
+            throw new \Exception(sprintf('No routes found for bus %s', $this->busName));
+        }
+        return new SendersLocator($config['routes'], $container);
+    }
+}
---
+++ classes/Gems/Messenger/TransportFactory.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Gems\Messenger;
+
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Messenger\Exception\TransportException;
+use Symfony\Component\Messenger\Transport\InMemoryTransport;
+use Symfony\Component\Messenger\Bridge\Redis\Transport\RedisTransport;
+use Symfony\Component\Messenger\Bridge\Redis\Transport\Connection as RedisConnection;
+use Symfony\Component\Messenger\Bridge\Doctrine\Transport\DoctrineTransport;
+use Symfony\Component\Messenger\Bridge\Doctrine\Transport\PostgreSqlConnection;
+use Symfony\Component\Messenger\Bridge\Doctrine\Transport\Connection;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Symfony\Component\Messenger\Transport\Serialization\PhpSerializer;
+use Symfony\Component\Messenger\Transport\TransportInterface;
+
+class TransportFactory implements FactoryInterface
+{
+
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null): TransportInterface
+    {
+        $config = $container->get('config');
+        if (!isset($config['messenger'], $config['messenger']['buses'], $config['messenger']['transports'][$requestedName])) {
+            throw new TransportException('No message transport config found');
+        }
+        $transportConfig = $config['messenger']['transports'][$requestedName];
+        if (!isset($transportConfig['dsn'])) {
+            throw new TransportException(sprintf('No transport dsn found for %s', $requestedName));
+        }
+        $dsnParts = explode('://', $transportConfig['dsn']);
+
+        switch($dsnParts) {
+            case 'in-memory':
+                return $this->getInMemoryTransport($transportConfig);
+            case 'redis':
+                return $this->getRedisTransport($transportConfig);
+            case 'doctrine':
+                return $this->getDoctrineTransport($transportConfig, $container);
+            default:
+                throw new \Exception(sprintf('Transport %s not supported', $dsnParts[0]));
+        }
+    }
+
+    protected function getDoctrineTransport(array $transportConfig, ContainerInterface $container)
+    {
+        if (!class_exists(DoctrineTransport::class)) {
+            throw new TransportException('Missing package "symfony/doctrine-messenger". Use `composer req symfony/doctrine-messenger` to fix this.');
+        }
+
+        try {
+            $entityManagerClass = EntityManagerInterface::class;
+            if (isset($transportConfig['entityManager']) && $container->has($transportConfig['entityManager'])) {
+                $entityManagerClass = $transportConfig['entityManager'];
+            }
+            $entityManager = $container->get($entityManagerClass);
+
+            $serializer = new PhpSerializer();
+            if (isset($transportConfig['serializer'])) {
+                $serializer = new $transportConfig['serializer']();
+            }
+
+            $driverConnection = $entityManager->getConnection();
+
+            $useNotify = true;
+            if (isset($transportConfig['use_notify'])) {
+                $useNotify = $transportConfig['use_notify'];
+            }
+
+            $configuration = PostgreSqlConnection::buildConfiguration($transportConfig['dsn'], $transportConfig);
+
+            if ($useNotify && $driverConnection->getDatabasePlatform() instanceof PostgreSQLPlatform) {
+                $connection = new PostgreSqlConnection($configuration, $driverConnection);
+            } else {
+                $connection = new Connection($configuration, $driverConnection);
+            }
+
+            return new DoctrineTransport($connection, $serializer);
+        } catch (\Throwable $exception) {
+            throw new TransportException('Missing dependency on the Doctrine ManagerRegistry!', 0, $exception);
+        }
+    }
+
+    protected function getInMemoryTransport(array $transportConfig): InMemoryTransport
+    {
+        $serializer = null;
+        if (isset($transportConfig['serializer'])) {
+            $serializer = new $transportConfig['serializer']();
+        }
+        return new InMemoryTransport($serializer);
+    }
+
+    protected function getRedisTransport(array $transportConfig): RedisTransport
+    {
+        if (!class_exists(RedisTransport::class)) {
+            throw new TransportException('Missing package "symfony/redis-messenger". Use `composer req symfony/redis-messenger` to fix this.');
+        }
+
+        return new RedisTransport(RedisConnection::fromDsn($transportConfig['dsn']));
+    }
+}
