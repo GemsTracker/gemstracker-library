a2aa1a0aa84633493d9863bcf8298829a0fb92ee
Jasper van Gestel <jvangestel@gmail.com>
2022-09-29 17:45:09 +0200

    Add test source

---
+++ classes/Gems/Dev/configs/db/seeds/TestSource.php
@@ -0,0 +1,32 @@
+<?php
+
+class TestSource extends \Phinx\Seed\AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'gso_source_name' => 'test1',
+                'gso_ls_url' => 'http://gems-ls.test',
+                'gso_ls_class' => 'LimeSurvey5m00Database',
+                'gso_ls_adapter' => 'Mysqli',
+                'gso_ls_database' => 'pulse_ls',
+                'gso_ls_table_prefix' => 'ls__',
+                'gso_changed_by' => 1,
+                'gso_created_by' => 1
+            ],
+        ];
+
+        $sources = $this->table('gems__sources');
+        $sources->insert($data)
+            ->saveData();
+    }
+}
