355c921d27c8a73e855f54ad82265cd42dd7e778
Jasper van Gestel <jvangestel@gmail.com>
2022-11-16 13:43:45 +0100

    Add auto configurator

---
+++ classes/Gems/Config/AutoConfigurator.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Gems\Config;
+
+use ReflectionClass;
+use ReflectionException;
+use Symfony\Component\Finder\Finder;
+
+class AutoConfigurator
+{
+    protected ?Finder $finder = null;
+
+    protected $autoconfigConfig = [];
+
+    protected ?string $fileHash = null;
+
+    public function __construct(protected readonly array $config)
+    {}
+
+    protected function addChecksumToAutoConfig()
+    {
+        $this->autoconfigConfig['autoconfig']['checksum'] = $this->getFilesHash();
+    }
+
+    public function autoConfigure(bool $recreate = false): array
+    {
+        if (!$recreate && $this->isFresh()) {
+            return $this->config;
+        }
+
+        $finder = $this->getFinder();
+        foreach($finder as $file) {
+            $className = str_replace(
+                '\\\\',
+                '\\',
+                ('Gems\\' . str_replace(
+                        '/',
+                        '\\',
+                        $file->getRelativePath()
+                    ) . '\\' . $file->getFilenameWithoutExtension())
+            );
+            try {
+                $fileReflector = new ReflectionClass($className);
+            } catch (ReflectionException $e) {
+            }
+
+            $this->checkFileForAutoconfiguration($fileReflector);
+        }
+
+        $this->addChecksumToAutoConfig();
+
+        if (count($this->autoconfigConfig)) {
+            $this->writeAutoConfigConfig();
+            return $this->getNewConfig();
+        }
+        return $this->config;
+    }
+
+    protected function checkFileForAutoconfiguration(ReflectionClass $reflector): void
+    {
+        if (isset($this->config['autoconfig'], $this->config['autoconfig']['settings'])) {
+            $settings = $this->config['autoconfig']['settings'];
+            if (isset($settings['implements'])) {
+                foreach($settings['implements'] as $interfaceClass => $targetSettings) {
+                    if ($reflector->implementsInterface($interfaceClass) && !$reflector->isAbstract()) {
+                        $this->setSettings($reflector->getName(), $targetSettings);
+                    }
+                }
+            }
+            if (isset($settings['extends'])) {
+                foreach($settings['extends'] as $parentClass => $targetSettings) {
+                    if ($reflector->isSubclassOf($parentClass) && !$reflector->isAbstract()) {
+                        $this->setSettings($reflector->getName(), $targetSettings);
+                    }
+                }
+            }
+            if (isset($settings['attribute'])) {
+                foreach($settings['attribute'] as $attributeClass => $targetSettings) {
+                    if ($reflector->getAttributes($attributeClass) && !$reflector->isAbstract() && !$reflector->isInterface() && !$reflector->isTrait()) {
+                        $this->setSettings($reflector->getName(), $targetSettings);
+                    }
+                }
+            }
+        }
+    }
+
+    protected function getAutoconfigFilename(): string
+    {
+        return $this->config['autoconfig']['cache_path'];
+    }
+
+    /**
+     * @return
+     */
+    protected function getFilePaths(): array
+    {
+        $moduleConfigProviders = require('config/modules.php');
+        $filePaths = [];
+
+        foreach($moduleConfigProviders as $configProvider) {
+            $reflector = new ReflectionClass($configProvider);
+            $path = dirname($reflector->getFileName());
+            if (!in_array($path, $filePaths)) {
+                $filePaths[] = $path;
+            }
+        }
+        return $filePaths;
+    }
+
+    protected function getFinder(): Finder
+    {
+        if ($this->finder === null) {
+            $paths = $this->getFilePaths();
+            $this->finder = new Finder();
+            $this->finder->files()
+                ->in($paths)
+                ->name('*.php');
+        }
+
+        return $this->finder;
+    }
+
+    public function getFilesHash(): string
+    {
+        if ($this->fileHash === null) {
+            $checkValues = [];
+            $finder = $this->getFinder();
+            foreach ($finder as $file) {
+                $checkValues[] = $file->getPath() . $file->getFilenameWithoutExtension() . $file->getMtime();
+            }
+            $this->fileHash = crc32(join(':', $checkValues));
+        }
+        return $this->fileHash;
+    }
+
+    protected function getNewConfig(): array
+    {
+        return require $this->config['rootDir'] . '/config/config.php';
+    }
+
+    public function hasChangedFiles(string $currentChecksum): bool
+    {
+        $newChecksum = $this->getFilesHash();
+        if ($newChecksum === $currentChecksum) {
+            return false;
+        }
+        return true;
+    }
+
+    public function isFresh(): bool
+    {
+        if (!isset($this->config['autoconfig'], $this->config['autoconfig']['checksum'])) {
+            return false;
+        }
+        $env = $this->config['app']['env'];
+        if ($env !== 'development') {
+            return true;
+        }
+        return !$this->hasChangedFiles($this->config['autoconfig']['checksum']);
+    }
+
+    protected function setSettings(string $className, array $settings): void
+    {
+        if (isset($settings['config'])) {
+            $configNamespaceArray = explode('.', $settings['config']);
+
+            $result = [];
+            foreach(array_reverse($configNamespaceArray) as $namespaceItem) {
+                if (!$result) {
+                    $result = [$namespaceItem => [$className]];
+                    continue;
+                }
+                $result = [$namespaceItem => $result];
+            }
+
+            $this->autoconfigConfig = array_merge_recursive($this->autoconfigConfig, $result);
+        }
+    }
+
+    protected function writeAutoConfigConfig(): void
+    {
+        $filename = $this->getAutoconfigFilename();
+        file_put_contents($filename, '<?php return ' . var_export($this->autoconfigConfig, true) . ';');
+    }
+}
