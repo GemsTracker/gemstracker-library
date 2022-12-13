f6182d681a178637ebfaa12925b43a7012d3ed38
Jasper van Gestel <jvangestel@gmail.com>
2022-12-13 22:04:50 +0100

    Add datepicker form element

---
+++ classes/Gems/Form/Element/DatePicker.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Gems\Form\Element;
+
+use DateTimeInterface;
+use MUtil\Bootstrap\Form\Element\Text;
+use Zend_View_Interface;
+
+class DatePicker extends Text
+{
+
+    protected $_elementClass = 'form-control date-picker';
+
+    public function render(Zend_View_Interface $view = null)
+    {
+        if ($this->_value instanceof DateTimeInterface && isset($this->dateFormat)) {
+            $this->_value = $this->_value->format($this->dateFormat);
+        }
+
+        //unset($this->dateFormat);
+        unset($this->storageFormat);
+
+        return parent::render($view);
+    }
+}
