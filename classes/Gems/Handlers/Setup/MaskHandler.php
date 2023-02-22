338fa67336017b2645d253ad3c1a303c5aeada79
mddejong <mjong@magnafacta.nl>
2023-02-22 19:07:21 +0100

    Fixed working of date selector in Per Period Overview

---
+++ classes/Gems/Handlers/Setup/MaskHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ *
+ * @package    Gems
+ * @subpackage Handlers\Setup
+ * @author     Matijs de Jong <mjong@magnafacta.nl>
+ */
+
+namespace Gems\Handlers\Setup;
+
+/**
+ *
+ * @package    Gems
+ * @subpackage Handlers\Setup
+ * @since      Class available since version 1.9.2
+ */
+class MaskHandler
+{
+
+}
