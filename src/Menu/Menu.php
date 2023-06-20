b4211034d7b58b0753d15c263e62768c322cdc70
Roel van Meer <roel.van.meer@peercode.nl>
2023-06-20 12:15:48 +0200

    Menu: Add support for aliased menu items
    This allows us to open a menu tree for routes that don't have a
    dedicated menu item. The aliased route determines the menu tree that is
    opened.

--- src/Menu/Menu.php
+++ src/Menu/Menu.php
@@ -24,6 +24,7 @@ class Menu extends MenuNode
             $object = match($item['type']) {
                 'route', 'route-link-item' => new RouteLinkItem($item['name'], $item['label']),
                 'container' => new ContainerLinkItem($item['name'], $item['label']),
+                'alias' => new AliasItem($item['name'], $item['alias']),
                 default => throw new \Exception('Invalid type: ' . $item['type']),
             };
 
@@ -56,6 +57,10 @@ class Menu extends MenuNode
         return $this->items[$name] ?? throw new MenuItemNotFoundException($name);
     }
 
+    /**
+     * Make the menu item for this route and its parent items active.
+     * If the menu item is an alias, we open the aliased path instead.
+     */
     public function openRouteResult(RouteResult $routeResult): void
     {
         $name = $routeResult->getMatchedRouteName();
@@ -65,6 +70,12 @@ class Menu extends MenuNode
         }
 
         $item = $this->items[$name];
+        if ($item instanceof AliasItem) {
+            $alias = $item->alias;
+            if (isset($this->items[$alias])) {
+                $item = $this->items[$alias];
+            }
+        }
         $item->openPath($routeResult->getMatchedParams());
     }
 
