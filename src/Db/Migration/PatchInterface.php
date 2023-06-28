60ecb02acca49c9b7cef7bd1643236821a28631c
Jasper van Gestel <jvangestel@gmail.com>
2023-06-28 08:21:18 +0200

    Add Patches and update seeds and tables

---
+++ src/Db/Migration/PatchInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Gems\Db\Migration;
+
+interface PatchInterface
+{
+    /**
+     * Get optional patch description
+     *
+     * @return string|null
+     */
+    public function getDescription(): string|null;
+
+
+    /**
+     * Get order in which this patch should be run
+     *
+     * @return int
+     */
+    public function getOrder(): int;
+
+    /**
+     * Get array of sql queries of patches
+     *
+     * @return array
+     */
+    public function __invoke(): array;
+}
--- src/Event/DatabaseMigrationEvent.php
+++ src/Event/DatabaseMigrationEvent.php
@@ -2,9 +2,66 @@
 
 namespace Gems\Event;
 
+use DateTimeInterface;
+use Gems\Event\Application\EventDuration;
 use Symfony\Contracts\EventDispatcher\Event;
 
 class DatabaseMigrationEvent extends Event
 {
+    use EventDuration;
+    public function __construct(
+        private readonly string $type,
+        private readonly int|string $version,
+        private readonly string $module,
+        private readonly string $name,
+        private readonly string $status,
+        private readonly string $sql,
+        private readonly string|null $comment = null,
+        DateTimeInterface|float|null $start = null,
+        DateTimeInterface|float|null $end = null,
+    )
+    {
+        $this->start = $start;
+        $this->end = $end;
+    }
 
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return int|string
+     */
+    public function getVersion(): int|string
+    {
+        return $this->version;
+    }
+
+    /**
+     * @return string
+     */
+    public function getModule(): string
+    {
+        return $this->module;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSql(): string
+    {
+        return $this->sql;
+    }
 }
