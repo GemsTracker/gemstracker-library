b235007274d9d7699efde1473347c3f4afd4d4b0
Roel van Meer <roel.van.meer@peercode.nl>
2023-08-02 20:01:22 +0200

    [03/09] ratelimit: Allow configuration per route and request method
    Also refactor some checks into the prepare() function to simplify.

--- src/Middleware/RateLimitMiddleware.php
+++ src/Middleware/RateLimitMiddleware.php
@@ -17,26 +17,41 @@ use Psr\Http\Server\RequestHandlerInterface;
 use RuntimeException;
 
 /**
- * Limit the amount of requests that are possible on this endpoint, or api in general
+ * Limit the amount of requests that are possible on this endpoint, or api in
+ * general. Note that rate limiting will not work if no cache is configured,
+ * but we have no way of checking that here.
  *
  * Class RateLimitMiddleware
- * @package Auth\Middleware
+ * @package Gems\Middleware
  */
 class RateLimitMiddleware implements MiddlewareInterface
 {
+    private string $routeName;
+
+    private string $requestMethod;
+
     protected int $decayTimeInSeconds = 60;
 
     protected int $maxAttempts = 120;
 
 
     public function __construct(
-        protected readonly RateLimiter $limiter
+        protected readonly RateLimiter $limiter,
+        protected readonly array $config,
     )
     {
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        if (!$this->prepare($request)) {
+            throw new RuntimeException('Unable to prepare for rate limiting.');
+        }
+
+        if (!$this->needsRatelimit()) {
+            return $handler->handle($request);
+        }
+
         $key = $this->getRequestKey($request);
 
         if ($this->limiter->tooManyAttempts($key, $this->maxAttempts)) {
@@ -49,6 +64,26 @@ class RateLimitMiddleware implements MiddlewareInterface
     }
 
     /**
+     * Fill some variables we need to do our work.
+     * Returns false if anything is not in order.
+     *
+     * @param ServerRequestInterface $request
+     * @return boolean
+     */
+    private function prepare(ServerRequestInterface $request): bool
+    {
+        $routeResult = $request->getAttribute(RouteResult::class);
+        $route = $routeResult->getMatchedRoute();
+        if (!$route instanceof Route) {
+            return false;
+        }
+        $this->routeName = $route->getName();
+        $this->requestMethod = $request->getMethod();
+
+        return true;
+    }
+
+    /**
      * Get the request cache key based on the route and either the login
      * details or the IP address.
      *
@@ -56,10 +91,6 @@ class RateLimitMiddleware implements MiddlewareInterface
      */
     protected function getRequestKey(ServerRequestInterface $request): string
     {
-        $routeResult = $request->getAttribute(RouteResult::class);
-        $route = $routeResult->getMatchedRoute();
-        $method = $request->getMethod();
-
         // Now we check if this is an authenticated session.
         $currentIdentity = $request->getAttribute(AuthenticationMiddleware::CURRENT_IDENTITY_ATTRIBUTE);
         if (is_null($currentIdentity)) {
@@ -77,11 +108,84 @@ class RateLimitMiddleware implements MiddlewareInterface
 
         }
 
-        if ($route instanceof Route && !is_null($sessionPart)) {
-            $key = implode('.', [$route->getName(), $method, $sessionPart, 'rate-limit']);
+        if (!is_null($sessionPart)) {
+            $key = implode('.', [$this->routeName, $this->requestMethod, $sessionPart, 'rate-limit']);
             return sha1($key);
         }
 
         throw new RuntimeException('Unable to generate request key.');
+    }
+
+    /**
+     * Return true if we need to do rate limiting for this request.
+     * If we do, we also set the configuration for the rate limit.
+     *
+     * @return boolean
+     */
+    protected function needsRatelimit(): bool
+    {
+        $keys = $this->getConfigKeys();
+        foreach ($keys as $key) {
+            if (!isset($this->config['ratelimit'][$key])) {
+                continue;
+            }
+
+            return $this->parseConfig($this->config['ratelimit'][$key]);
+        }
+
+        return false;
+    }
+
+    /**
+     * Return an array of config keys we have to check in the rate limit config.
+     * We check the entire route name tree, with and without the request method
+     * appended, as well as defaults, so for a GET request to the 'foo.bar'
+     * route, we'll generate a list like this:
+     * - foo.bar.GET
+     * - foo.bar
+     * - foo.GET
+     * - foo
+     * - default.GET
+     * - default
+     *
+     * @return array<string>
+     */
+    private function getConfigKeys(): array
+    {
+        $parts = explode('.', $this->routeName);
+        for ($i = count($parts); $i > 0; $i--) {
+            $key = implode('.', array_slice($parts, 0, $i));
+            $configKeys[] = $key . '.' . $this->requestMethod;
+            $configKeys[] = $key;
+        }
+        $configKeys[] = 'default.' . $this->requestMethod;;
+        $configKeys[] = 'default';
+
+        return $configKeys;
+    }
+
+    /**
+     * Parse a ratelimit config value. The value can either be false to
+     * disable rate limiting for the route (and its children) or a string
+     * like '5/60', defined as <number of requests> per <time in seconds>.
+     *
+     * Also returns false if the value is incorrectly set.
+     *
+     * @param string|false $configValue
+     * @return boolean
+     */
+    private function parseConfig(string|false $configValue): bool
+    {
+        if ($configValue === false) {
+            return false;
+        }
+        $values = explode('/', $configValue, 2);
+        if (count($values) != 2) {
+            return false;
+        }
+        $this->maxAttempts = intval($values[0]);
+        $this->decayTimeInSeconds = intval($values[1]);
+
+        return true;
     }
 }
