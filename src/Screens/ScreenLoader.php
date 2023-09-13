b706169285e976248956c524594f01cbc104f900
Roel van Meer <roel.van.meer@peercode.nl>
2023-09-13 09:17:07 +0200

    [2/5] screens: Refactor and type declarations of ScreenLoader
    Based on the implementation in TrackEvents.

--- src/Screens/ScreenLoader.php
+++ src/Screens/ScreenLoader.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  *
  * @package    Gems
@@ -11,6 +13,10 @@
 
 namespace Gems\Screens;
 
+use Gems\Cache\HelperAdapter;
+use Gems\Exception\Coding;
+use Gems\Util\Translated;
+use ReflectionClass;
 use Zalt\Loader\ProjectOverloader;
 
 /**
@@ -23,14 +29,14 @@ use Zalt\Loader\ProjectOverloader;
  */
 class ScreenLoader
 {
-    const RESPONDENT_BROWSE_SCREEN      = 'Respondent\\Browse';
-    const RESPONDENT_EDIT_SCREEN        = 'Respondent\\Edit';
-    const RESPONDENT_SHOW_SCREEN        = 'Respondent\\Show';
-    const RESPONDENT_SUBSCRIBE_SCREEN   = 'Respondent\\Subscribe';
-    const RESPONDENT_UNSUBSCRIBE_SCREEN = 'Respondent\\Unsubscribe';
-    const TOKEN_ASK_SCREEN              = 'Token\\Ask';
+    const RESPONDENT_BROWSE_SCREEN      = 'Respondent/Browse';
+    const RESPONDENT_EDIT_SCREEN        = 'Respondent/Edit';
+    const RESPONDENT_SHOW_SCREEN        = 'Respondent/Show';
+    const RESPONDENT_SUBSCRIBE_SCREEN   = 'Respondent/Subscribe';
+    const RESPONDENT_UNSUBSCRIBE_SCREEN = 'Respondent/Unsubscribe';
+    const TOKEN_ASK_SCREEN              = 'Token/Ask';
 
-    protected $config;
+    protected array $config;
 
     /**
      * Each screen type must implement an screen class or interface derived
@@ -50,9 +56,16 @@ class ScreenLoader
         ];
 
     public function __construct(
+        protected readonly Translated $translatedUtil,
+        protected readonly HelperAdapter $cache,
         protected readonly ProjectOverloader $overloader,
+        array $config,
     )
-    {}
+    {
+        if (isset($config['screens'])) {
+            $this->config = $config['screens'];
+        }
+    }
 
     /**
      * Lookup screen class for a screen type. This class or interface should at the very least
@@ -62,52 +75,44 @@ class ScreenLoader
      *
      * @param string $screenType The type (i.e. lookup directory) to find the associated class for
      * @return string Class/interface name associated with the type
+     * @throws Coding If screenType is not supported.
      */
-    protected function _getScreenClass($screenType)
+    protected function _getScreenClass(string $screenType): string
     {
         if (isset($this->_screenClasses[$screenType])) {
             return $this->_screenClasses[$screenType];
         } else {
-            throw new \Gems\Exception\Coding("No screen class exists for screen type '$screenType'.");
+            throw new Coding("No screen class exists for screen type '$screenType'.");
         }
-    }
-
-    /**
-     *
-     * @param string $screenType An screen subdirectory (may contain multiple levels split by '/'
-     * @return array An array of type prefix => classname
-     */
-    protected function _getScreenDirs($screenType)
-    {
-        $paths = [];
-        if (DIRECTORY_SEPARATOR == '/') {
-            $screenDir = str_replace('\\', DIRECTORY_SEPARATOR, $screenType);
-        } else {
-            $screenDir = $screenType;
-        }
-        foreach ($this->_dirs as $name => $dir) {
-            $prefix = $name . '\\Screens\\'. $screenType . '\\';
-            $fullPath = $dir . DIRECTORY_SEPARATOR . 'Screens' . DIRECTORY_SEPARATOR . $screenDir;
-            if (file_exists($fullPath)) {
-                $paths[$prefix] = $fullPath;
-            }
-        }
-
-        return $paths;
     }
 
     /**
      * Returns a list of selectable screens with an empty element as the first option.
      *
      * @param string $screenType The type (i.e. lookup directory with an associated class) of the screens to list
-     * @return ScreenInterface or more specific a $screenClass type object
+     * @return array of ScreenInterface or more specific a $screenClass type object
      */
-    protected function _listScreens($screenType)
+    protected function _listScreens(string $screenType): array
     {
-        $screenClass = $this->_getScreenClass($screenType);
-        $paths       = $this->_getScreenDirs($screenType);
+        $key = HelperAdapter::cleanupForCacheId(static::class . 'listScreens_' . $screenType);
+        if ($this->cache->hasItem($key)) {
+            return $this->translatedUtil->getEmptyDropdownArray() + $this->cache->getCacheItem($key);
+        }
 
-        return $this->listClasses($screenClass, $paths, 'getScreenLabel');
+        $screenClasses = $this->getScreenClasses($screenType);
+
+        $screenList = [];
+        if ($screenClasses) {
+            foreach ($screenClasses as $screenName) {
+                $screen = $this->_loadSCreen($screenName, $screenType);
+                $screenList[$screenName] = $screen->getScreenLabel() . " ({$screenName})";
+            }
+        }
+        asort($screenList);
+
+        $this->cache->setCacheItem($key, $screenList);
+
+        return $this->translatedUtil->getEmptyDropdownArray() + $screenList;
     }
 
     /**
@@ -116,25 +121,39 @@ class ScreenLoader
      * @param string $screenName The class name of the individual screen to load
      * @param string $screenType The type (i.e. lookup directory with an associated class) of the screen
      * @return ScreenInterface or more specific a $screenClass type object
+     * @throws Coding If screen name and screen type do not match
      */
-    protected function _loadScreen($screenName, $screenType)
+    protected function _loadScreen(string $screenName, string $screenType): ScreenInterface
     {
         $screenClass = $this->_getScreenClass($screenType);
 
         $screen = $this->overloader->create($screenName);
 
         if (! $screen instanceof $screenClass) {
-            throw new \Gems\Exception\Coding("The screen '$screenName' of type '$screenType' is not an instance of '$screenClass'.");
+            throw new Coding("The screen '$screenName' of type '$screenType' is not an instance of '$screenClass'.");
         }
 
         return $screen;
     }
 
     /**
+     * @param string $screenType
+     * @return string[]
+     */
+    protected function getScreenClasses(string $screenType): array
+    {
+        if (isset($this->config[$screenType])) {
+            return $this->config[$screenType];
+        }
+
+        return [];
+    }
+
+    /**
      *
      * @return array screenname => string
      */
-    public function listRespondentBrowseScreens()
+    public function listRespondentBrowseScreens(): array
     {
         return $this->_listScreens(self::RESPONDENT_BROWSE_SCREEN);
     }
@@ -143,7 +162,7 @@ class ScreenLoader
      *
      * @return array screenname => string
      */
-    public function listRespondentEditScreens()
+    public function listRespondentEditScreens(): array
     {
         return $this->_listScreens(self::RESPONDENT_EDIT_SCREEN);
     }
@@ -152,7 +171,7 @@ class ScreenLoader
      *
      * @return array screenname => string
      */
-    public function listRespondentShowScreens()
+    public function listRespondentShowScreens(): array
     {
         return $this->_listScreens(self::RESPONDENT_SHOW_SCREEN);
     }
@@ -161,16 +180,16 @@ class ScreenLoader
      *
      * @return array screenname => string
      */
-    public function listSubscribeScreens()
+    public function listSubscribeScreens(): array
     {
-        return \Gems\Util\Translated::$emptyDropdownArray + $this->_listScreens(self::RESPONDENT_SUBSCRIBE_SCREEN);
+        return $this->_listScreens(self::RESPONDENT_SUBSCRIBE_SCREEN);
     }
 
     /**
      *
      * @return array screenname => string
      */
-    public function listTokenAskScreens()
+    public function listTokenAskScreens(): array
     {
         return $this->_listScreens(self::TOKEN_ASK_SCREEN);
     }
@@ -179,9 +198,9 @@ class ScreenLoader
      *
      * @return array screenname => string
      */
-    public function listUnsubscribeScreens()
+    public function listUnsubscribeScreens(): array
     {
-        return \Gems\Util\Translated::$emptyDropdownArray + $this->_listScreens(self::RESPONDENT_UNSUBSCRIBE_SCREEN);
+        return $this->_listScreens(self::RESPONDENT_UNSUBSCRIBE_SCREEN);
     }
 
     /**
@@ -189,7 +208,7 @@ class ScreenLoader
      * @param string $screenName Name of the screen class
      * @return BrowseScreenInterface
      */
-    public function loadRespondentBrowseScreen($screenName)
+    public function loadRespondentBrowseScreen(string $screenName): BrowseScreenInterface
     {
         return $this->_loadScreen($screenName, self::RESPONDENT_BROWSE_SCREEN);
     }
@@ -199,7 +218,7 @@ class ScreenLoader
      * @param string $screenName Name of the screen class
      * @return EditScreenInterface
      */
-    public function loadRespondentEditScreen($screenName)
+    public function loadRespondentEditScreen(string $screenName): EditScreenInterface
     {
         return $this->_loadScreen($screenName, self::RESPONDENT_EDIT_SCREEN);
     }
@@ -209,17 +228,17 @@ class ScreenLoader
      * @param string $screenName Name of the screen class
      * @return ShowScreenInterface
      */
-    public function loadRespondentShowScreen($screenName)
+    public function loadRespondentShowScreen(string $screenName): ShowScreenInterface
     {
         return $this->_loadScreen($screenName, self::RESPONDENT_SHOW_SCREEN);
     }
 
     /**
      *
-     * @param string $screenName Name of the screen class
+     * @param string|null $screenName Name of the screen class
      * @return SubscribeScreenInterface|null
      */
-    public function loadSubscribeScreen($screenName)
+    public function loadSubscribeScreen(string|null $screenName): SubscribeScreenInterface|null
     {
         if ($screenName) {
             return $this->_loadScreen($screenName, self::RESPONDENT_SUBSCRIBE_SCREEN);
@@ -232,17 +251,17 @@ class ScreenLoader
      * @param string $screenName Name of the screen class
      * @return AskScreenInterface
      */
-    public function loadTokenAskScreen($screenName)
+    public function loadTokenAskScreen(string $screenName): AskScreenInterface
     {
         return $this->_loadScreen($screenName, self::TOKEN_ASK_SCREEN);
     }
 
     /**
      *
-     * @param string $screenName Name of the screen class
+     * @param string|null $screenName Name of the screen class
      * @return UnsubscribeScreenInterface|null
      */
-    public function loadUnsubscribeScreen($screenName)
+    public function loadUnsubscribeScreen(string|null $screenName): UnsubscribeScreenInterface|null
     {
         if ($screenName) {
             return $this->_loadScreen($screenName, self::RESPONDENT_UNSUBSCRIBE_SCREEN);
