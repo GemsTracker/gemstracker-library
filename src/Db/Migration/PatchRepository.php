06673ce72c92521564903b7816f36510cb89c0c4
Roel van Meer <roel.van.meer@peercode.nl>
2023-09-26 14:17:11 +0200

    [27/36] PatchRepository: Document possible issue

--- src/Db/Migration/PatchRepository.php
+++ src/Db/Migration/PatchRepository.php
@@ -170,6 +170,8 @@ class PatchRepository extends MigrationRepositoryAbstract
         try {
             if (!$connection->inTransaction()) {
                 $connection->beginTransaction();
+                // FIXME: This should be set but setting it breaks things.
+                // $localTransaction = true;
             }
             foreach($patchInfo['sql'] as $sqlQuery) {
                 $resultFetcher->query($sqlQuery);
