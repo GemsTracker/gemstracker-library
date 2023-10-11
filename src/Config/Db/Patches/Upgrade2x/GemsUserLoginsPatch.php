3bb2e0bcd5f3cd041309e3fe9015919ae3600453
Jasper van Gestel <jvangestel@gmail.com>
2023-10-11 20:51:00 +0200

    gul_enable_2factor is now not nullable

--- src/Config/Db/Patches/Upgrade2x/GemsUserLoginsPatch.php
+++ src/Config/Db/Patches/Upgrade2x/GemsUserLoginsPatch.php
@@ -19,6 +19,7 @@ class GemsUserLoginsPatch extends PatchAbstract
     public function up(): array
     {
         return [
+            "UPDATE gems__user_logins SET gul_enable_2factor = 0 WHERE gul_enable_2factor IS NULL",
             "ALTER TABLE gems__user_logins MODIFY COLUMN gul_enable_2factor tinyint(1) NOT NULL DEFAULT '1'",
             'ALTER TABLE gems__user_logins ADD COLUMN gul_session_key varchar(32) DEFAULT NULL AFTER gul_can_login',
         ];
