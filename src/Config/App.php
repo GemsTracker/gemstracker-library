46aa54b31cb6e6f68a546ff306e37584630a27ab
Jasper van Gestel <jvangestel@gmail.com>
2023-10-11 20:51:08 +0200

    use getenv

--- src/Config/App.php
+++ src/Config/App.php
@@ -9,8 +9,8 @@ class App
         return [
             'name' => 'GemsTracker',
             'description' => 'GEneric Medical Survey Tracker',
-            'env' => isset($_ENV['APP_ENV']) ? $_ENV['APP_ENV'] : null,
-            'key' => isset($_ENV['APP_KEY']) ? $_ENV['APP_KEY'] : null,
+            'env' => getenv('APP_ENV'),
+            'key' => getenv('APP_KEY'),
         ];
     }
 }
