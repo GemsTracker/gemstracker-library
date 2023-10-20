9beefe12b2787d55c4067d679a75d9557b49be36
Jasper van Gestel <jvangestel@gmail.com>
2023-10-20 12:20:45 +0200

    Check for DSN in env first

--- src/Factory/MailerFactory.php
+++ src/Factory/MailerFactory.php
@@ -12,17 +12,14 @@ use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mailer\Transport;
 use Symfony\Component\Mailer\Transport\SendmailTransport;
+use Symfony\Component\Mailer\Transport\TransportInterface;
 
 class MailerFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null): MailerInterface
     {
         $config = $container->get('config');
-        if (isset($config['mail'], $config['mail']['dsn'])) {
-            $transport = Transport::fromDsn($config['mail']['dsn']);
-        } else {
-            $transport = new SendmailTransport();
-        }
+        $transport = $this->getTransport($config);
 
         // Load the MailBouncer to enable it
         $container->get(MailBouncer::class);
@@ -30,5 +27,18 @@ class MailerFactory implements FactoryInterface
         $mailer = new Mailer($transport);
 
         return $mailer;
+    }
+
+    protected function getTransport(array $config): TransportInterface
+    {
+        $envDsn = getenv('MAILER_DSN') ?? null;
+        if ($envDsn) {
+            return Transport::fromDsn($envDsn);
+        }
+        if (isset($config['mail']['dsn'])) {
+            return Transport::fromDsn($config['mail']['dsn']);
+        }
+
+        return new SendmailTransport();
     }
 }
