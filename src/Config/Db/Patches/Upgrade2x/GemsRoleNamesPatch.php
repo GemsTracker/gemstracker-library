4004a04bfbc368d571924e9bfbfc59b6bf023156
Roel van Meer <roel.van.meer@peercode.nl>
2023-10-24 09:10:01 +0200

    Add GemsRoleNamesPatch
    Role names should be lowercase and should not contain dashes or
    underscores.

---
+++ src/Config/Db/Patches/Upgrade2x/GemsRoleNamesPatch.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Gems\Config\Db\Patches\Upgrade2x;
+
+use Gems\Db\Migration\PatchAbstract;
+
+class GemsRoleNamesPatch extends PatchAbstract
+{
+    public function getDescription(): string|null
+    {
+        return 'Update role names for Gemstracker 2.x';
+    }
+
+    public function getOrder(): int
+    {
+        return 20230102000201;
+    }
+
+    public function up(): array
+    {
+        return [
+            'UPDATE gems__roles SET grl_name=LOWER(REPLACE(REPLACE(grl_name,"_",""),"-",""))',
+            'UPDATE gems__groups SET ggp_role=LOWER(REPLACE(REPLACE(ggp_role,"_",""),"-",""))',
+        ];
+    }
+}
