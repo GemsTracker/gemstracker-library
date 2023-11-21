7d53db9425c15eff4b39a7e06cd8482d89ad99a8
Roel van Meer <roel.van.meer@peercode.nl>
2023-11-21 15:58:00 +0100

    DatabaseInfo: Code consistency

--- src/Db/Migration/DatabaseInfo.php
+++ src/Db/Migration/DatabaseInfo.php
@@ -15,8 +15,9 @@ class DatabaseInfo
     public function tableExists(string $tableName): bool
     {
         $metaData = Factory::createSourceFromAdapter($this->adapter);
-        return in_array($tableName, $metaData->getTableNames());
+        $tables = $metaData->getTableNames();
 
+        return in_array($tableName, $tables);
     }
 
     public function tableHasColumn(string $tableName, string $columnName): bool
