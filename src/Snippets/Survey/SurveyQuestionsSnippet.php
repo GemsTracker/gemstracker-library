703526900278fba0bf6f60647c4877c4c6681d0c
Roel van Meer <roel.van.meer@peercode.nl>
2023-11-28 09:38:41 +0100

    SurveyQuestionsSnippet: Get survey of specific round if so requested

--- src/Snippets/Survey/SurveyQuestionsSnippet.php
+++ src/Snippets/Survey/SurveyQuestionsSnippet.php
@@ -231,8 +231,15 @@ class SurveyQuestionsSnippet extends TableSnippetAbstract
             }
 
             if ($this->trackId && (! $this->surveyId)) {
-                // Use the track ID to get the id of the first active survey
-                $this->surveyId = intval($this->resultFetcher->fetchOne('SELECT gro_id_survey FROM gems__rounds WHERE gro_active = 1 AND gro_id_track = ? ORDER BY gro_id_order', [$this->trackId]));
+                // If a specific round was requested, show that. Otherwise, show
+                // the survey of the first round.
+                $roundId = $this->requestInfo->getParam('rid');
+                if ($roundId) {
+                    $this->surveyId = intval($this->resultFetcher->fetchOne('SELECT gro_id_survey FROM gems__rounds WHERE gro_id_track = ? AND gro_id_round = ?', [$this->trackId, $roundId]));
+                } else {
+                    // Use the track ID to get the id of the first active survey
+                    $this->surveyId = intval($this->resultFetcher->fetchOne('SELECT gro_id_survey FROM gems__rounds WHERE gro_active = 1 AND gro_id_track = ? ORDER BY gro_id_order', [$this->trackId]));
+                }
             }
         }
         // \MUtil\EchoOut\EchoOut::track($this->surveyId, $this->trackId);
