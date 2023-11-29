08f6d7b3188f40e9bca63ef4a15b5bf1b51dbcbe
Jasper van Gestel <jvangestel@gmail.com>
2023-11-29 11:33:44 +0100

    Vue create/edit snippet can be a lot simpeler

--- src/SnippetsActions/Vue/CreateAction.php
+++ src/SnippetsActions/Vue/CreateAction.php
@@ -3,8 +3,10 @@
 namespace Gems\SnippetsActions\Vue;
 
 use Gems\Snippets\Vue\CreateEditSnippet;
+use Zalt\SnippetsActions\AbstractAction;
+use Zalt\SnippetsActions\PostActionInterface;
 
-class CreateAction extends \Gems\SnippetsActions\Form\CreateAction
+class CreateAction extends AbstractAction implements PostActionInterface
 {
     /**
      * @var array Of snippet class names
