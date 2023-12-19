af46614a11b517738ad0288d4fc8cad070c0ca0b
Jasper van Gestel <jvangestel@gmail.com>
2023-12-19 20:21:17 +0100

    Update addTrackFieldsByCodeTransformer

--- src/Tracker/Model/AddTrackFieldsByCodeTransformer.php
+++ src/Tracker/Model/AddTrackFieldsByCodeTransformer.php
@@ -2,54 +2,39 @@
 
 namespace Gems\Tracker\Model;
 
+use Gems\Tracker;
+use Zalt\Model\MetaModel;
 use Zalt\Model\MetaModelInterface;
+use Zalt\Model\Transform\ModelTransformerAbstract;
 
-class AddTrackFieldsByCodeTransformer extends \MUtil\Model\ModelTransformerAbstract
+class AddTrackFieldsByCodeTransformer extends ModelTransformerAbstract
 {
 
-
-    protected $includeCodes;
-    
-    /**
-     *
-     * @var string The field that contains the respondent track id
-     */
-    protected $respTrackIdField = 'gr2t_id_respondent_track';
-
-    /**
-     * @var \Gems\Tracker\TrackerInterface
-     */
-    protected $tracker;
-
-    protected $trackFieldsByRespondentTrack;
+    protected array $trackFieldsByRespondentTrack = [];
 
     /**
      *
-     * @param \Gems\Tracker\TrackerInterface $tracker
+     * @param Tracker $tracker
      * @param array $includeCodes
-      * @param $respTrackIdField Overwrite the default field that contains the respondent track id (gr2t_id_respondent_track)
+      * @param string $respTrackIdField Overwrite the default field that contains the respondent track id (gr2t_id_respondent_track)
      */
-    public function __construct(\Gems\Tracker\TrackerInterface $tracker, array $includeCodes, $respTrackIdField = false)   
-    {
-        $this->includeCodes = $includeCodes;
-        $this->tracker = $tracker;
-        
-        if ($respTrackIdField) {
-            $this->respTrackIdField = $respTrackIdField;
-        }
-    }
+    public function __construct(
+        protected readonly Tracker $tracker,
+        protected readonly array $includeCodes,
+        protected readonly string $respTrackIdField = 'gr2t_id_respondent_track')
+    {}
 
     /**
      * The transform function performs the actual transformation of the data and is called after
      * the loading of the data in the source model.
      *
-     * @param \MUtil\Model\ModelAbstract $model The parent model
+     * @param MetaModelInterface $model The parent model
      * @param array $data Nested array
      * @param boolean $new True when loading a new item
      * @param boolean $isPostData With post data, unselected multiOptions values are not set so should be added
      * @return array Nested array containing (optionally) transformed data
      */
-    public function transformLoad(MetaModelInterface $model, array $data, $new = false, $isPostData = false)
+    public function transformLoad(MetaModelInterface $model, array $data, $new = false, $isPostData = false): array
     {
         foreach($data as $tokenId=>$row) {
             if (isset($row[$this->respTrackIdField])) {
@@ -67,7 +52,7 @@ class AddTrackFieldsByCodeTransformer extends \MUtil\Model\ModelTransformerAbstr
         return $data;
     }
 
-    protected function getTrackfields($trackId, $respondentTrackId)
+    protected function getTrackFields(int $trackId, int $respondentTrackId): array
     {
         $engine = $this->tracker->getTrackEngine($trackId);
 
