a5caefcb71aed5f652d7686782566a0f1844f50c
Roel van Meer <roel.van.meer@peercode.nl>
2024-02-06 16:59:27 +0100

    Tables: Ensure tables can be created with db:tables command
    Order tables such that referenced tables are created before tables with
    foreign keys referencing those tables, and ensure column types are
    correct.

--- configs/db/tables/gems__user_logins.22.sql
+++ configs/db/tables/gems__user_logins.22.sql
@@ -5,7 +5,7 @@ CREATE TABLE if not exists gems__user_logins (
         gul_id_user          bigint unsigned not null auto_increment,
 
         gul_login            varchar(30) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' not null,
-        gul_id_organization  bigint not null references gems__organizations (gor_id_organization),
+        gul_id_organization  bigint unsigned not null references gems__organizations (gor_id_organization),
 
         gul_user_class       varchar(30) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci' not null default 'NoLogin',
         gul_can_login        boolean not null default 0,
