b30116fe6cd8c3c6dcdee12c631a74239c9227e1
Jasper van Gestel <jvangestel@gmail.com>
2024-04-18 11:08:50 +0200

    Update CookieResponse default values and add organization id cookie on login

--- src/CookieResponse.php
+++ src/CookieResponse.php
@@ -2,23 +2,36 @@
 
 namespace Gems;
 
+use DateTimeInterface;
 use Dflydev\FigCookies\FigResponseCookies;
 use Dflydev\FigCookies\Modifier\SameSite;
 use Dflydev\FigCookies\SetCookie;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Zalt\Base\RequestUtil;
 
 class CookieResponse
 {
     protected static array $defaultOptions = [
         'httpOnly' => true,
-        'sameSite' => 'strict', // strict, lax or none
+        'sameSite' => 'lax', // strict, lax or none
         'path' => '/',
         // 'domain' => '',
         // 'expires' => '' // int|string|DateTimeInterface
     ];
 
-    public static function addCookieToResponse(ServerRequestInterface $request, ResponseInterface $response, string $key, mixed $value, array $options = []): ResponseInterface
+    public static function addCookieToResponse(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        string $key,
+        mixed $value,
+        bool $httpOnly = true,
+        string $sameSite = 'lax',
+        string|null $path = '/',
+        int $maxAge = 90 * 86400, // 90 days
+        DateTimeInterface|null $expires = null,
+        string|null $domain = null,
+    ): ResponseInterface
     {
         if (empty($options)) {
             $options = static::$defaultOptions;
@@ -28,46 +41,42 @@ class CookieResponse
             ->withValue($value)
             ->withoutSameSite();
 
-        if (isset($options['httpOnly']) && $options['httpOnly'] === true) {
+        if ($httpOnly) {
             $cookie = $cookie->withHttpOnly();
         }
-        if (isset($options['sameSite'])) {
-            switch ($options['sameSite']) {
-                case 'strict':
-                    $cookie = $cookie->withSameSite(SameSite::strict());
-                    break;
-                case 'lax':
-                    $cookie = $cookie->withSameSite(SameSite::lax());
-                    break;
-                case 'none':
-                    $cookie = $cookie->withSameSite(SameSite::none());
-                    break;
-                default:
-                    break;
-            }
+
+        switch ($sameSite) {
+            case 'strict':
+                $cookie = $cookie->withSameSite(SameSite::strict());
+                break;
+            case 'lax':
+                $cookie = $cookie->withSameSite(SameSite::lax());
+                break;
+            case 'none':
+                $cookie = $cookie->withSameSite(SameSite::none());
+                break;
+            default:
+                break;
         }
 
-        if (isset($options['path'])) {
-            $cookie = $cookie->withPath($options['path']);
+        if ($path !== null) {
+            $cookie = $cookie->withPath($path);
         }
 
-        if (isset($options['domain'])) {
-            $cookie = $cookie->withDomain($options['domain']);
+        if ($domain !== null) {
+            $cookie = $cookie->withDomain($domain);
         }
 
-        if (isset($options['expires'])) {
-            $cookie = $cookie->withExpires($options['expires']);
+        if ($expires !== null) {
+            $cookie = $cookie->withExpires($expires);
         }
 
-        if (isset($options['maxAge'])) {
-            $cookie = $cookie->withMaxAge($options['maxAge']);
+        $cookie = $cookie->withMaxAge($maxAge);
+
+        if (RequestUtil::isSecure($request)) {
+            $cookie = $cookie->withSecure();
         }
 
-        // TODO: Check for secure connection
-        // $cookie = $cookie->withSecure();
-
-        $response = FigResponseCookies::set($response, $cookie);
-
-        return $response;
+        return FigResponseCookies::set($response, $cookie);
     }
 }
