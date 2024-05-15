b765930f3155fd428cb37c9e4f11ad9066520b09
Roel van Meer <roel.van.meer@peercode.nl>
2024-05-15 11:26:38 +0200

    Add FileBrowseSearchAction.php
    Apparently I forgot to commit this.

---
+++ src/SnippetsActions/Browse/FileBrowseSearchAction.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ *
+ * @package    Gems
+ * @subpackage SnippetsActions
+ */
+
+namespace Gems\SnippetsActions\Browse;
+
+/**
+ *
+ * @package    Gems
+ * @subpackage SnippetsActions
+ * @since      Class available since version 2.x
+ */
+class FileBrowseSearchAction extends BrowseSearchAction
+{
+    public array $menuEditRoutes = ['download'];
+}
