883f51319c68f3596d01b67f143ddedaff366da9
Jasper van Gestel <jvangestel@gmail.com>
2024-06-13 11:16:29 +0200

    use a decrypted string to get access token and put it in a request for further validation

--- src/Handlers/Auth/AccessTokenSessionLoginHandler.php
+++ src/Handlers/Auth/AccessTokenSessionLoginHandler.php
@@ -2,11 +2,15 @@
 
 namespace Gems\Handlers\Auth;
 
+use Defuse\Crypto\Crypto;
+use Defuse\Crypto\Key;
 use Gems\AuthNew\Adapter\AccessTokenAuthentication;
 use Gems\AuthNew\AuthenticationServiceBuilder;
+use Gems\Helper\Env;
 use Gems\User\UserLoader;
 use Laminas\Diactoros\Response\JsonResponse;
 use Laminas\Diactoros\Response\RedirectResponse;
+use Laminas\Diactoros\ServerRequest;
 use League\OAuth2\Server\ResourceServer;
 use Mezzio\Helper\UrlHelper;
 use Mezzio\Session\SessionMiddleware;
@@ -16,7 +20,7 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class AccessTokenSessionLoginHandler implements RequestHandlerInterface
 {
-    private string $payloadParamName = 'forward';
+    private string $payloadParamName = 'key';
 
     private string $defaultEntryRoute;
 
@@ -33,18 +37,50 @@ class AccessTokenSessionLoginHandler implements RequestHandlerInterface
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        $queryParams = $request->getQueryParams();
+        if (!isset($queryParams[$this->payloadParamName])) {
+            return new JsonResponse(
+                [
+                    'error' => 'authentication_failed',
+                    'message' => 'No payload found',
+                ],
+                401
+            );
+        }
+        $decryptedPayload = $this->decryptPayload($queryParams[$this->payloadParamName]);
+        if ($decryptedPayload === null) {
+            return new JsonResponse(
+                [
+                    'error' => 'authentication_failed',
+                    'message' => 'Payload could not be decrypted',
+                ],
+                401
+            );
+        }
+
+        $accessTokenRequest = new ServerRequest(
+            method: 'GET',
+            headers: [
+                'authorization' => $decryptedPayload['access_token']
+            ],
+        );
+
         $session = $request->getAttribute(SessionMiddleware::SESSION_ATTRIBUTE);
         $authenticationService = $this->authenticationServiceBuilder->buildAuthenticationService($session);
         $authenticationResult = $authenticationService->authenticate(
             new AccessTokenAuthentication(
                 $this->resourceServer,
-                $request,
+                $accessTokenRequest,
                 $this->userLoader,
             )
         );
 
         if ($authenticationResult->isValid()) {
-            return $this->getRedirectResponse($request);
+            $forwardUrl = $decryptedPayload['forward_url'] ?? null;
+            if (!$forwardUrl) {
+                $forwardUrl = $this->urlHelper->generate($this->defaultEntryRoute);
+            }
+            return new RedirectResponse($forwardUrl);
         }
 
         return new JsonResponse(
@@ -56,34 +92,16 @@ class AccessTokenSessionLoginHandler implements RequestHandlerInterface
         );
     }
 
-    protected function getRedirectResponse(ServerRequestInterface $request): ResponseInterface
+    protected function decryptPayload(string $payload): array|null
     {
-        $forwardUrl = match ($request->getMethod()) {
-            'POST' => $this->getForwardUrlFromPost($request),
-            'GET' => $this->getForwardUrlFromGet($request),
-            default => null,
-        };
-
-        if ($forwardUrl === null) {
-            $forwardUrl = $this->urlHelper->generate($this->defaultEntryRoute);
+        if (Env::get('R_DECRYPTION_KEY') === null) {
+            return null;
         }
-
-        return new RedirectResponse($forwardUrl);
-    }
-
-    protected function getForwardUrlFromGet(ServerRequestInterface $request): string|null
-    {
-        $params = $request->getQueryParams();
-        if (isset($params[$this->payloadParamName])) {
-            return base64_decode($params[$this->payloadParamName]);
+        try {
+            $result = Crypto::decrypt($payload, Key::loadFromAsciiSafeString(Env::get('R_DECRYPTION_KEY')));
+            return json_decode($result, true);
+        } catch (\Throwable) {
         }
         return null;
-    }
-
-    protected function getForwardUrlFromPost(ServerRequestInterface $request): string|null
-    {
-        $rawBody = $request->getBody()->getContents();
-        $body = json_decode($rawBody, true);
-        return $body[$this->payloadParamName] ?? null;
     }
 }
