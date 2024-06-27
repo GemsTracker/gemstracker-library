cd16f1dfa07c60d1e63dc59ec0df46e42dd3ed73
Jasper van Gestel <jvangestel@gmail.com>
2024-06-27 10:16:29 +0200

    Add ability to use Env as placeholder and render it when needed from config

--- src/Helper/Env.php
+++ src/Helper/Env.php
@@ -4,8 +4,27 @@ namespace Gems\Helper;
 
 class Env
 {
+    public function __construct(
+        public string $name
+    )
+    {}
+
     public static function get(string $name, string|null $default = null): string|null
     {
         return $_ENV[$name] ?? $_SERVER[$name] ?? $default;
+    }
+
+    public static function renderConfig(array $config): array
+    {
+        foreach($config as $configKey => $configValue) {
+            if (is_array($configValue)) {
+                $config[$configKey] = static::renderConfig($configValue);
+            }
+            if ($configValue instanceof Env) {
+                $config[$configKey] = static::get($configValue->name);
+            }
+        }
+
+        return $config;
     }
 }
