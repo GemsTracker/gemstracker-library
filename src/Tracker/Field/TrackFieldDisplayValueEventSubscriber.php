1fc9ece2fa60dfecf6e05cfb469782ef9adcd4be
Jasper van Gestel <jvangestel@gmail.com>
2024-09-11 16:08:00 +0200

    add appointment field data

--- src/Tracker/Field/TrackFieldDisplayValueEventSubscriber.php
+++ src/Tracker/Field/TrackFieldDisplayValueEventSubscriber.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Gems\Tracker\Field;
 
+use Gems\Agenda\Agenda;
 use Gems\Agenda\Repository\AgendaStaffRepository;
 use Gems\Agenda\Repository\LocationRepository;
 use Gems\Event\TrackFieldDisplayValueEvent;
@@ -35,10 +36,25 @@ class TrackFieldDisplayValueEventSubscriber implements EventSubscriberInterface
         $event->displayValue = match($event->type) {
             'caretaker' => $this->getCaretakerName((int)$event->rawValue),
             'location' => $this->getLocationName((int)$event->rawValue),
+            'appointment' => $this->getAppointmentValue((int)$event->rawValue),
             default => $event->displayValue,
         };
 
         // Do not stop propagation to allow overwrites of above display values
+    }
+
+    private function getAppointmentValue(int $appointmentId): ?string
+    {
+        /**
+         * @var Agenda $agenda
+         */
+        $agenda = $this->container->get(Agenda::class);
+        $appointment = $agenda->getAppointment($appointmentId);
+        if ($appointment->exists) {
+            return $appointment->getAdmissionTime()->format($agenda->appointmentDisplayFormat);
+        }
+
+        return null;
     }
 
     private function getCaretakerName(int $caretakerId): ?string
