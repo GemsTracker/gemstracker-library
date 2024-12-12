d2a7ed083cafae0a41b138fa226e7d48e3acd70b
Jasper van Gestel <jvangestel@gmail.com>
2024-12-12 23:02:39 +0100

    Also add fixed values on load

--- src/Model/Transform/FixedValueTransformer.php
+++ src/Model/Transform/FixedValueTransformer.php
@@ -15,6 +15,16 @@ class FixedValueTransformer extends ModelTransformerAbstract
     {
     }
 
+    public function transformLoad(MetaModelInterface $model, array $data, $new = false, $isPostData = false): array
+    {
+        foreach($data as $key=>$row) {
+            foreach($this->fixedFieldValues as $column => $value) {
+                $data[$key][$column] = $value;
+            }
+        }
+        return $data;
+    }
+
     public function transformRowBeforeSave(MetaModelInterface $model, array $row): array
     {
         foreach($this->fixedFieldValues as $column => $value) {
