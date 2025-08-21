f516f5f7d0f1d90a77385a2463a37718b3dd3117
Jasper van Gestel <jvangestel@gmail.com>
2025-08-21 10:24:04 +0200

    Fix for normal url's not being allowed in TwigAttributeSanitizer and update twig attribute identifier

--- src/HtmlSanitizer/TwigAttributeSanitizer.php
+++ src/HtmlSanitizer/TwigAttributeSanitizer.php
@@ -6,9 +6,21 @@ namespace Gems\HtmlSanitizer;
 
 use Symfony\Component\HtmlSanitizer\HtmlSanitizerConfig;
 use Symfony\Component\HtmlSanitizer\Visitor\AttributeSanitizer\AttributeSanitizerInterface;
+use Symfony\Component\HtmlSanitizer\Visitor\AttributeSanitizer\UrlAttributeSanitizer;
+use Twig\Environment;
+use Twig\Error\Error;
+use Twig\Loader\ArrayLoader;
+use Twig\Node\PrintNode;
+use Twig\Source;
 
 class TwigAttributeSanitizer implements AttributeSanitizerInterface
 {
+    public function __construct(
+        private Environment|null $twig = null,
+    )
+    {
+    }
+
     public function getSupportedElements(): ?array
     {
         return ['a'];
@@ -25,10 +37,48 @@ class TwigAttributeSanitizer implements AttributeSanitizerInterface
         string $value,
         HtmlSanitizerConfig $config
     ): ?string {
-        if (str_starts_with($value, '{{') && str_ends_with($value, '}}')) {
+
+        $this->initTwigEnvironment();
+
+        if ($this->isTwigPrintTag($value)) {
             return $value;
         }
 
-        return null;
+        // Handle as normal url attribute. UrlAttributeSanitizer is final. UrlSanitizer is internal,
+        // so we can't extend or just do the UrlSanitizing directly
+        $urlAttributeSanitizer = new UrlAttributeSanitizer();
+        return $urlAttributeSanitizer->sanitizeAttribute($element, $attribute, $value, $config);
+    }
+
+    // Only init twig once when it is not set on construct, and only when we actually need it
+    private function initTwigEnvironment(): void
+    {
+        if ($this->twig === null) {
+            $loader = new ArrayLoader();
+            $this->twig = new Environment($loader, []);
+        }
+    }
+
+    private function isTwigPrintTag(string $value): bool
+    {
+        try {
+            $source = new Source($value, 'inline_template');
+            $stream = $this->twig->tokenize($source);
+            $nodes = $this->twig->parse($stream);
+        } catch (Error) {
+            return false;
+        }
+
+        $body = $nodes->getNode('body');
+
+        if ($body->count() !== 1) {
+            return false;
+        }
+
+        $firstNode = $body->getNode('0');
+        if ($firstNode instanceof PrintNode) {
+            return true;
+        }
+        return false;
     }
 }
