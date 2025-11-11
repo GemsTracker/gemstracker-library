adb2c6faf53049f6e2b987ea6217ff345631d652
mddejong <mjong@magnafacta.nl>
2025-11-11 15:31:58 +0100

    Added options for specifying a home page and using Ldap

---
+++ src/AuthNew/Adapter/LdapAuthentication.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @package    Gems
+ * @subpackage AuthNew\Adapter
+ * @author     Matijs de Jong <mjong@magnafacta.nl>
+ */
+
+namespace Gems\AuthNew\Adapter;
+
+/**
+ * @package    Gems
+ * @subpackage AuthNew\Adapter
+ * @since      Class available since version 1.0
+ */
+class LdapAuthentication
+{
+
+}
