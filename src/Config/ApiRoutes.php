da34d87d5d6066e26331afd6bf09f0c2b6444c57
Jasper van Gestel <jvangestel@gmail.com>
2026-03-19 11:21:50 +0100

    Instead of adding custom middleware to every group, we pipe those middleware on runtime, making the route preperation faster

--- src/Config/ApiRoutes.php
+++ src/Config/ApiRoutes.php
@@ -22,13 +22,14 @@ class ApiRoutes extends RestModelConfigProviderAbstract
     {
         return [
             ...$this->routeGroup(
+                'api',
                 [
                     'path' => $this->pathPrefix,
-                    'middleware' => $this->getMiddleware(),
                 ],
                 $this->getRoutes()
             ),
             ...$this->routeGroup(
+                'public-api',
                 [
                     'path' => $this->pathPrefix,
                     'middleware' => [
---
+++ src/Route/MiddlewareGroup.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Gems\Route;
+
+use Laminas\Stratigility\MiddlewarePipe;
+use Mezzio\Router\RouteResult;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class MiddlewareGroup implements MiddlewareInterface
+{
+    public function __construct(
+        private readonly ContainerInterface $container,
+        private readonly array $groups,
+    )
+    {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        /** @var RouteResult $routeResult */
+        $routeResult = $request->getAttribute(RouteResult::class);
+        $routeGroup = $routeResult->getMatchedRoute()->getOptions()['routeGroup'] ?? null;
+
+        if (!$routeGroup || empty($this->groups[$routeGroup]['middleware'])) {
+            return $handler->handle($request);
+        }
+
+        $pipeline = new MiddlewarePipe();
+        foreach($this->groups[$routeGroup]['middleware'] as $middlewareClassName) {
+            $pipeline->pipe($this->container->get($middlewareClassName));
+        }
+        return $pipeline->process($request, $handler);
+    }
+}
