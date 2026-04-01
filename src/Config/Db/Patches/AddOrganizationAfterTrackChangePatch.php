96a2df043f3eb63acc3a68b3c02d71096056ad93
Roel van Meer <roel.van.meer@peercode.nl>
2026-04-01 14:26:24 +0200

    [2/3] Check if columns exist before adding them
    This fixes migrations when run on a fresh setup.

--- src/Config/Db/Patches/AddOrganizationAfterTrackChangePatch.php
+++ src/Config/Db/Patches/AddOrganizationAfterTrackChangePatch.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 namespace Gems\Config\Db\Patches;
 
 use Gems\Config\ConfigAccessor;
+use Gems\Db\Migration\DatabaseInfo;
 use Gems\Db\Migration\PatchAbstract;
 
 /**
@@ -20,7 +21,10 @@ use Gems\Db\Migration\PatchAbstract;
  */
 class AddOrganizationAfterTrackChangePatch extends PatchAbstract
 {
-    public function __construct(protected readonly ConfigAccessor $configAccessor)
+    public function __construct(
+        protected readonly DatabaseInfo $databaseInfo,
+        protected readonly ConfigAccessor $configAccessor
+    )
     {  }
 
     public function getDescription(): string|null
@@ -35,9 +39,11 @@ class AddOrganizationAfterTrackChangePatch extends PatchAbstract
 
     public function up(): array
     {
-        return [
-            "ALTER TABLE gems__organizations ADD gor_track_change_route varchar(255) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' null default null AFTER gor_token_ask;",
-            sprintf("UPDATE gems__organizations SET gor_track_change_route = '%s' WHERE gor_track_change_route IS NULL;", $this->configAccessor->getAfterTrackChangeDefaultRoute()),
-            ];
+        $statements = [];
+        if (!$this->databaseInfo->tableHasColumn('gems__organizations', 'gor_track_change_route')) {
+            $statements[] = "ALTER TABLE gems__organizations ADD gor_track_change_route varchar(255) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci' null default null AFTER gor_token_ask";
+        }
+        $statements[] = sprintf("UPDATE gems__organizations SET gor_track_change_route = '%s' WHERE gor_track_change_route IS NULL;", $this->configAccessor->getAfterTrackChangeDefaultRoute());
+        return $statements;
     }
 }
