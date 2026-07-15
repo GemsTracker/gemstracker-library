e8e63de1c1f48eddc85535dba8b521b6720208ef
mddejong <mjong@magnafacta.nl>
2026-07-15 16:10:54 +0200

    One more fix for unit tests on translations

--- src/Translate/DbTranslationRepository.php
+++ src/Translate/DbTranslationRepository.php
@@ -88,7 +88,7 @@ class DbTranslationRepository
             if ($key) {
                 $row = $this->translateRow($tableName, $key, $row);
             } elseif (is_array($row) && 0 == $row[$keyField]) {
-                $row = $this->translateRow($tableName, 0, $row);
+                $row = $this->translateRow($tableName, "0", $row);
             }
         }
         return $data;
